23c9883baf587363ad9feec306bf6169c1c805a2
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2022-12-22 13:30:55 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (74.77%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
@@ -1,21 +1,16 @@
 class Solution {
     public int arraySign(int[] nums) {
-        int count = 0;
         
-        /* In this count the total number of negative number 
-         If it is divisible by 2 which means it is positive
-        If at any point if the element is 0 simply return 0
-        Else return -1.*/
+        int count_negatives = 0;
         
         for(int i=0; i<nums.length; i++){
-               if(nums[i]==0)
-                   return 0;
+           if(nums[i]==0)
+               return 0;
             
-                else if(nums[i]<0)
-                    count++;
+          else if(nums[i]<0)
+              count_negatives++;
         }
         
-       
-     return (count%2==0)?1:-1;
+        return (count_negatives%2==0)?1:-1;
     }
 }
