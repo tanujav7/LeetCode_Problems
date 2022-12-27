0bfb0cd9d5bcfc794d3e11a8bab58cd1a3017083
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2022-12-27 12:59:30 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (76.32%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,6 +1,7 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
         int left = 0;
+        //In this left pointer will be pointing to the Non value(val) element whereas right pointer will traverse through the array.
         
         for(int right=0; right<nums.length; right++){
             if(nums[right]!=val){
