64bf020613a665ff3be64185e5435b4d9827bce4
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-01-01 20:37:06 +0530

    Time: 3 ms (78.41%), Space: 42.3 MB (90.62%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -3,43 +3,40 @@ class Solution {
         //For output
       HashSet <Integer> res = new HashSet<>();
         
-       //For binary search atleast one array needs to be sorted
-        Arrays.sort(nums1);
+      Arrays.sort(nums1);
+      Arrays.sort(nums2);
         
-        for(int i=0; i<nums2.length; i++){
-             if(binarySearch(nums1, nums1.length, nums2[i])==true)
-                 res.add(nums2[i]);
+    //Two pointer initialization
+        
+        int i=0;
+        int j=0;
+        
+        while(i<nums1.length && j<nums2.length){
+           
+            if(nums1[i]==nums2[j]){
+                res.add(nums1[i]);
+                i++;
+                j++;
+            }
+            
+            else if(nums1[i]<nums2[j]){
+                i++;
+            }
+            
+            else{
+                j++;
+            }
         }
         
-        
         int output[] = new int[res.size()];
-        int j=0;
+        int index=0;
         //Copying the elements of Hashset to array using for each loop
         for(int k:res){
-            output[j++] = k;
+            output[index++] = k;
         }
         
         return output;
     }
-    
-    public boolean binarySearch(int arr[], int n, int Key){
-         int start = 0;
-         int end = n-1;
-        
-        while(start<=end){
-            int mid = start+(end-start)/2;
-            
-            if(arr[mid]==Key)
-                return true;
-            
-            else if(arr[mid]<Key)
-                start = mid+1;
-            
-            else
-                end = mid-1;
-        }
-        
-        return false;
-    }
+
 }
 
