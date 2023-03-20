9af57dfb36705cd6325714da399bfb36582f0a81
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-03-21 00:20:06 +0530

    Time: 97 ms, Memory: 46.5 MB - LeetHub

--- count-primes/count-primes.java
+++ count-primes/count-primes.java
@@ -2,17 +2,22 @@ class Solution {
     public int countPrimes(int n) {
       
          int count = 0;
-
+   //To store the boolean values to prove whether a number is prime or not, we initialize a bool array.
+    //The array is initialized with false value.
       boolean bool[] = new boolean[n];
         
         for(int i=2; i<Math.sqrt(n); i++){
                     
             if(bool[i]==false){
-                    
+            
                 for(int j=i*i; j<n; j=j+i)
                     bool[j] = true;
             }
         }
+        
+        
+        //If the number is prime then the bool[i] = false;
+        //If the number is composite then bool[i] = true;
         
         for(int i=2; i<n; i++){
                 
