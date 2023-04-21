56044437b506ddae301583f3fa9175c17a14f913
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-04-21 10:24:59 +0530

    Time: 20 ms (37.73%), Space: 58.9 MB (17.21%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -4,15 +4,11 @@ class Solution {
         Arrays.sort(nums);
         
         for(int i=0; i<nums.length-1; i++){
-
+            
             if(nums[i]==nums[i+1])
                 return true;
-        
         }
-        
-        
+            
         return false;
-            
-            
     }
 }
