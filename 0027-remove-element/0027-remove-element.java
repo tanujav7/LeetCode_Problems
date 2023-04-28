a994f2b11968785d308d6a1c47dad303c7e3539d
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-04-28 11:32:40 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (19.07%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,6 +1,8 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
+      
         int left = 0;
+        
         //In this left pointer will be pointing to the Non value(val) element whereas right pointer will traverse through the array.
         
         for(int right=0; right<nums.length; right++){
@@ -9,6 +11,7 @@ class Solution {
                 left++;
             }
         }
-        return left;
+       
+       return left;
     }
 }
