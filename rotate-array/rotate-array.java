dc168546dd8923f99803990a71b037081c351544
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-04-28 16:04:50 +0530

    Time: 0 ms, Memory: 58.8 MB - LeetHub

---
+++ rotate-array/rotate-array.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+         k = k%n;
+        
+        
+        reversalAlgorithm(nums, 0, n-k-1);
+        reversalAlgorithm(nums, n-k, n-1);
+        reversalAlgorithm(nums, 0, n-1);
+        
+    }
+    
+    void reversalAlgorithm(int arr[], int start, int end){
+       
+        while(start<end){
+         int temp = arr[start];
+         arr[start] = arr[end];
+         arr[end] = temp;
+         start++;
+         end--;
+        }
+        
+    }
+}
