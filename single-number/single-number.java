a366f6e8f6fc4415128b209ab24c409f084b4b94
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-04-28 16:07:17 +0530

    Time: 1 ms, Memory: 42.8 MB - LeetHub

---
+++ single-number/single-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        
+       int sum = 0;
+        
+        for(int i=0; i<nums.length; i++){
+                sum = sum^nums[i];
+        }
+        return sum;
+    }
+}
