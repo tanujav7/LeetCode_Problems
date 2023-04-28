6e79bafffb0d900ff5410b02154e1ccc8a1e4115
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-04-28 16:17:51 +0530

    Time: 1 ms, Memory: 39.5 MB - LeetHub

---
+++ number-of-1-bits/number-of-1-bits.java
@@ -0,0 +1,19 @@
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        
+        int count = 0;
+        
+        int mask = 1;
+        
+        for(int i=0; i<32; i++){
+
+            if((n & mask)==1)
+                count++;
+            
+            n = n>>1;
+        }
+        
+        return count;
+    }
+}
