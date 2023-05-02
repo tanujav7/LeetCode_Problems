1cfa92fc02459482e1b4df03fefc8d0a9fd01a41
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-05-02 12:19:22 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (65.10%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.java
@@ -1,16 +1,17 @@
 class Solution {
     public int arraySign(int[] nums) {
         
-        int count_negatives = 0;
-        
+        int countNegatives = 0;
+       
         for(int i=0; i<nums.length; i++){
-           if(nums[i]==0)
-               return 0;
+
+            if(nums[i]==0)
+                return 0;
             
-          else if(nums[i]<0)
-              count_negatives++;
+            if(nums[i]<0)
+                countNegatives++;
+
         }
-        
-        return (count_negatives%2==0)?1:-1;
+        return (countNegatives%2==0)?1:-1;
     }
 }
