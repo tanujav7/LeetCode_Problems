235c3ec3fd521ed65b6701b4936cd239a5345fb8
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-05-10 13:44:17 +0530

    Time: 41 ms (46.69%), Space: 53.1 MB (41.18%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -2,12 +2,11 @@ class Solution {
     public int[] sortArray(int[] nums) {
         int left = 0;
         int right = nums.length-1;
-        mergeSort(nums,left,right);
+         mergeSort(nums,left,right);
         return nums;
     }
     
     void mergeSort(int arr[], int left, int right){
-
         if(left>=right)
             return;
         
@@ -16,44 +15,34 @@ class Solution {
         mergeSort(arr,left,mid);
         mergeSort(arr,mid+1,right);
         merge(arr,left,mid,right);
-
     }
     
     void merge(int arr[], int left, int mid, int right){
-
-        int n1 = mid-left+1;
+        int n1 = mid+1-left;
         int n2 = right-mid;
         
-        int leftArray[] = new int[n1];
-        int rightArray[] = new int[n2];
-        
+        int leftArr[] = new int[n1];
+        int rightArr[] = new int[n2];
         
         for(int i=0; i<n1; i++)
-            leftArray[i] = arr[i+left];
+            leftArr[i] = arr[i+left];
         
         for(int i=0; i<n2; i++)
-            rightArray[i] = arr[mid+1+i];
+            rightArr[i] = arr[mid+1+i];
         
-        int i = 0;
-        int j = 0;
-        int k = left;
-        
+        int i=0, j=0, k=left;
         
         while(i<n1 && j<n2){
-
-            if(leftArray[i]<=rightArray[j])
-                arr[k++] = leftArray[i++];
-            
+            if(leftArr[i]<rightArr[j])
+                arr[k++] = leftArr[i++];
             else
-                arr[k++] = rightArray[j++];
-            
+                arr[k++] = rightArr[j++];
         }
-    
         
         while(i<n1)
-            arr[k++] = leftArray[i++];
+            arr[k++] = leftArr[i++];
         
         while(j<n2)
-            arr[k++] = rightArray[j++];
+            arr[k++] = rightArr[j++];
     }
 }
