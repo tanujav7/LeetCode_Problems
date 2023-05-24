e866df8d769eaab7cc14459570b126f65a8c4458
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-05-24 11:40:00 +0530

    Time: 0 ms (100.00%), Space: 45.1 MB (5.63%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -1,29 +1,26 @@
 class Solution {
     public int search(int[] nums, int target) {
         
- 
-        if(nums.length==1 && nums[0]==target)
-            return 0;
-        
-        int left = 0;
-        int right = nums.length-1;
-        
-        while(left<=right){
+       int left = 0;
+       int right = nums.length-1;
+       return binarySearch_Recursive(nums,left,right,target);
+    }
 
-            int mid = left+(right-left)/2;
-            
-            if(nums[mid]==target)
-                return mid;
-            
-            else if(nums[mid]<target)
-                left = mid+1;
-            
-            else
-                right = mid-1;
+    int binarySearch_Recursive(int nums[], int left, int right, int target){
+      if(left>right)
+         return -1;
 
-        }
-        
-        return -1;
-        
+      else{
+        int mid = left+(right-left)/2;
+
+        if(nums[mid]<target)
+        return binarySearch_Recursive(nums, mid+1,right,target);
+      
+        else if(nums[mid]>target)
+        return binarySearch_Recursive(nums,left, mid-1,target);
+
+        else
+         return mid;
+      }
     }
 }
