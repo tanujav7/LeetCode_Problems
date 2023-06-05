d2f2d737b2f470263aaa19f20b1bda55f05ee530
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-06-05 13:46:30 +0530

    Time: 1 ms (80.82%), Space: 45.3 MB (6.40%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
@@ -14,19 +14,17 @@ class Solution {
         while(head!=null && head.val==val)
             head = head.next;
         
-        ListNode currentNode = head;
+         ListNode currentNode = head;
         
         while(currentNode!=null && currentNode.next!=null){
-            
+           
             if(currentNode.next.val==val)
                 currentNode.next = currentNode.next.next;
             
             else
                 currentNode = currentNode.next;
-            
         }
         
         return head;
-        
     }
 }
