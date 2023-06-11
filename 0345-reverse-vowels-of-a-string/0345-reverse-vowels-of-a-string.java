c4fdb40cdf7c317564c1dea9c243d336b84c644c
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-06-11 14:53:45 +0530

    Time: 4 ms (78.62%), Space: 44 MB (46.54%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,24 +1,23 @@
 class Solution {
     public String reverseVowels(String s) {
-     
-        Set<Character> set = new HashSet<>();
         
+        Set<Character> set = new HashSet<>();
         set.add('a');
         set.add('e');
         set.add('i');
         set.add('o');
         set.add('u');
-        
         set.add('A');
         set.add('E');
         set.add('I');
         set.add('O');
         set.add('U');
         
-        char arr[] = s.toCharArray();   
+        char arr[] = s.toCharArray();
         
-        int start = 0;
-        int end = arr.length-1;
+        //Using two pointers
+        
+        int start = 0, end = arr.length-1;
         
         while(start<end){
             
@@ -29,18 +28,17 @@ class Solution {
                 end--;
             
             else{
-                
                 char temp = arr[start];
                 arr[start] = arr[end];
                 arr[end] = temp;
                 start++;
                 end--;
             }
-            
         }
         
         String res = new String(arr);
+        
         return res;
-       
+        
     }
 }
