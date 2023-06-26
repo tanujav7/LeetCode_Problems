92e2bf24084a197b99f7e99e7e0b5e953a7bb4d2
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-06-26 14:22:21 +0530

    Time: 1 ms, Memory: 39.5 MB - LeetHub

--- sqrtx/sqrtx.java
+++ sqrtx/sqrtx.java
@@ -1,22 +1,18 @@
 class Solution {
     public int mySqrt(int x) {
-        
-        int low = 1;
-        int high = x;
+        int low = 1, high = x;
         
         while(low<=high){
-            int mid = low+(high-low)/2;
+            int mid = (low)+(high-low)/2;
             
             long sq = (long)mid*(long)mid;
             
-            if(sq==x)
-                return mid;
-            
+            if(sq<x)
+                low = mid+1;
             else if(sq>x)
                 high = mid-1;
-            
             else
-                low = mid+1;
+                return mid;
         }
         
         return high;
