130d4f06fc11e459af083ab9677bd99462e218a9
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-07-01 17:07:13 +0530

    Time: 3 ms (97.93%), Space: 42 MB (83.44%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -3,7 +3,7 @@ class Solution {
        
          if(s==null){
         return false;
-    }
+      }
  
     s = s.toLowerCase();
  
@@ -11,13 +11,11 @@ class Solution {
     int j=s.length()-1;
  
     while(i<j){
-        while(i<j && !((s.charAt(i)>='a' && s.charAt(i)<='z') 
-                    || (s.charAt(i)>='0'&&s.charAt(i)<='9'))){
+        while(i<j && !((s.charAt(i)>='a' && (s.charAt(i)<='z')  || (s.charAt(i)>='0'&&s.charAt(i)<='9')))){
             i++;
         }
  
-        while(i<j && !((s.charAt(j)>='a' && s.charAt(j)<='z') 
-                    || (s.charAt(j)>='0'&&s.charAt(j)<='9'))){
+        while(i<j && !((s.charAt(j)>='a' && (s.charAt(j)<='z')  || (s.charAt(j)>='0'&&s.charAt(j)<='9')))){
             j--;
         }
  
