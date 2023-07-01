359c4269827b2a7fcd28c5d97962a707f5089cf5
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-07-01 12:25:05 +0530

    Time: 1 ms (100.00%), Space: 39.8 MB (64.93%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -5,18 +5,20 @@ class Solution {
         int remainder = 0;
         
         while(x!=0){
+            
             remainder = x%10;
             
-            if(reverse>Integer.MAX_VALUE/10 || (reverse == Integer.MAX_VALUE && remainder >7))
+            if(reverse>Integer.MAX_VALUE/10 || reverse==Integer.MAX_VALUE && remainder > 7)
                 return 0;
             
-            if(reverse<Integer.MIN_VALUE/10 || (reverse == Integer.MIN_VALUE && remainder < -8))
+            if(reverse<Integer.MIN_VALUE /10|| reverse==Integer.MIN_VALUE && remainder < -8)
                 return 0;
             
-            reverse = reverse*10 + remainder;
+            reverse = reverse * 10 + remainder;
             x = x/10;
+            
         }
-        
+      
         return reverse;
     }
 }
