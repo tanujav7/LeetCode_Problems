8c32030bf92d8fa6c32d47043c33e94898bff10b
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-07-01 13:38:46 +0530

    Time: 3 ms (97.93%), Space: 42.1 MB (78.61%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,22 +1,34 @@
 class Solution {
     public boolean isPalindrome(String s) {
-         if(s.isEmpty())         // if String s is empty return true
-        return true; 
-
-        String str = s.toLowerCase();// convert the whole string into lower case alphabet
-     
-    //remove non-alphanumeric characters
-    // replace the given string with empty string except the pattern [^a-zA-Z0-9]
-    str = str.replaceAll("[^a-zA-Z0-9]", "");
-     
-    //The reverse() method of StringBuilder is used to reverse the characters in the StringBuilder.
-    StringBuilder revstr = new StringBuilder(str);
-    revstr.reverse();
-    String rstr = revstr.toString();
-     
-    if(str.equals(rstr))//if string and reversed string both are equal return true
-        return true;
-     
-    return false;      
+       
+         if(s==null){
+        return false;
+    }
+ 
+    s = s.toLowerCase();
+ 
+    int i=0;
+    int j=s.length()-1;
+ 
+    while(i<j){
+        while(i<j && !((s.charAt(i)>='a' && s.charAt(i)<='z') 
+                    || (s.charAt(i)>='0'&&s.charAt(i)<='9'))){
+            i++;
+        }
+ 
+        while(i<j && !((s.charAt(j)>='a' && s.charAt(j)<='z') 
+                    || (s.charAt(j)>='0'&&s.charAt(j)<='9'))){
+            j--;
+        }
+ 
+        if(s.charAt(i) != s.charAt(j)){
+            return false;
+        }
+ 
+        i++;
+        j--;
+    }
+ 
+    return true;
     }
 }
