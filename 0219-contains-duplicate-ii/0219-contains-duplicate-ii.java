9279e244cf1c8ef1f4b4708ea8fcc00c130bddfe
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-07-06 21:15:36 +0530

    Time: 23 ms (25.47%), Space: 55.9 MB (48.59%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,18 +1,15 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         
-     HashMap<Integer, Integer> map = new HashMap<>();
         
-        for(int i=0; i<nums.length; i++){
-            if(map.containsKey(nums[i])){
-                int index = map.get(nums[i]);
-                if(Math.abs(i-index)<=k)
-                    return true;
-            }
-            
-            map.put(nums[i],i);
+Set<Integer> set = new HashSet<>();
+    for (int i = 0; i < nums.length; ++i) {
+        if (set.contains(nums[i])) return true;
+        set.add(nums[i]);
+        if (set.size() > k) {
+            set.remove(nums[i - k]);
         }
-        
-        return false;
+    }
+    return false;
     }
 }
