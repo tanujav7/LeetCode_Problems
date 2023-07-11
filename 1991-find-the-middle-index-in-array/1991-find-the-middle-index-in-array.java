3102d94333478ae6b0df763b31dc80a177a20652
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-07-11 10:33:17 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (97.25%) - LeetHub

---
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findMiddleIndex(int[] nums) {
+        int left_sum = 0;
+        int sum = 0;
+       
+        for(int n : nums)
+            sum+=n;
+        
+        for(int i=0; i<nums.length; i++){
+            if(left_sum==sum-left_sum-nums[i])
+                return i;
+            left_sum +=nums[i];
+        }
+        
+        return -1;
+    }
+}
