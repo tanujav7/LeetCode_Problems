674f8e00da076b17572fd01553d6e378c3a84d58
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-07-16 23:45:35 +0530

    Time: 1 ms (99.98%), Space: 56.1 MB (28.88%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,25 +1,21 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
         
-        //Two pointers
+        int sum = 0;
         int left = 0;
+        int minSubArray = Integer.MAX_VALUE;
         
-        //left will help in shrinking the window
-        //right will go through the array
-       
-        int currentWindowSum = 0;
-        int res = Integer.MAX_VALUE;
-        
-        for(int right = 0; right<nums.length; right++){
-            currentWindowSum +=nums[right];
+        for(int right=0; right<nums.length; right++){
+            sum+=nums[right];
             
-            while(currentWindowSum>=target){
-                currentWindowSum = currentWindowSum - nums[left];
-                res = Math.min(res,right-left+1);
+            while(sum>=target){
+                sum-=nums[left];
+                minSubArray = Math.min(minSubArray,right-left+1);
                 left++;
             }
         }
         
-        return res==Integer.MAX_VALUE ? 0 : res;
+        return (minSubArray==Integer.MAX_VALUE) ? 0 : minSubArray;
+            
     }
 }
