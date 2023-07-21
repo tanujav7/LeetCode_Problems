f0d2f9ab621bbd0a0254809468c09603cd8c81b2
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-07-21 18:37:06 +0530

    Time: 4 ms (69.06%), Space: 42 MB (76.69%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,20 +1,19 @@
 class Solution {
     public boolean isPalindrome(String s) {
         
-     s = s.toLowerCase();
+        s = s.toLowerCase();
+        int i=0, j=s.length()-1;
         
-     int i=0, j=s.length()-1;
-        
-     while(i<j){
-         while(i<j && !(s.charAt(i)>='a' && s.charAt(i)<='z' || (s.charAt(i)>='0' && s.charAt(i)<='9')))
-             i++;
-         while(i<j && !(s.charAt(j)>='a' && s.charAt(j)<='z' || (s.charAt(j)>='0' && s.charAt(j)<='9')))
-             j--;
-         if(s.charAt(i)!=(s.charAt(j)))
-             return false;
-         i++;
-         j--;
-     }
+        while(i<j){
+            while(i<j && !((s.charAt(i)>='a' && s.charAt(i)<='z') || s.charAt(i)>='0' && s.charAt(i)<='9'))
+                i++;
+            while(i<j && !((s.charAt(j)>='a' && s.charAt(j)<='z') || s.charAt(j)>='0' && s.charAt(j)<='9'))
+                j--;
+            if(s.charAt(i)!=s.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
         
         return true;
     }
