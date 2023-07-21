94f9d70ec8e73fddb209f99b76b012116982d05b
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-07-21 20:49:36 +0530

    Time: 1091 ms (5.62%), Space: 44.7 MB (88.05%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -3,7 +3,7 @@ class Solution {
         //Approach 1 with better solution
         //Approach 4
         //Approach 5
-        int dup = -1, missing = 1;
+    /*    int dup = -1, missing = 1;
         for (int n: nums) {
             if (nums[Math.abs(n) - 1] < 0)
                 dup = Math.abs(n);
@@ -14,7 +14,25 @@ class Solution {
             if (nums[i] > 0)
                 missing = i + 1;
         }
-        return new int[]{dup, missing};
+        return new int[]{dup, missing};*/
         
+        int n = nums.length;
+        int duplicate = -1;
+        int missing = -1;
+        
+        for(int i=1; i<=n; i++){
+            int count = 0;
+            for(int j=0; j<n; j++){
+                if(i==nums[j])
+                    count++;
+            }
+            
+            if(count==2)
+                duplicate = i;
+            if(count==0)
+                missing = i;
+        }
+        
+        return new int[] {duplicate, missing};
     }
 }
