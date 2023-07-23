608767e80dad8a2fbf337bc2441771907aacb2a1
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-07-23 19:08:52 +0530

    Time: 197 ms (51.10%), Space: 45.6 MB (46.79%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -1,38 +1,20 @@
 class Solution {
     public String removeStars(String s) {
         
-   /*     Stack<Character> stack = new Stack<>();
-        StringBuilder str = new StringBuilder();
-        char arr[] = s.toCharArray();
+        Stack<Character> stack = new Stack();
         
-        for(int i=0; i<arr.length; i++){
+        for(int i=0; i<s.length(); i++){
             if(s.charAt(i)=='*')
-               stack.pop();
+                stack.pop();
             else
                 stack.push(s.charAt(i));
         }
         
+        StringBuilder str = new StringBuilder();
+        
         while(!stack.isEmpty())
             str.append(stack.pop());
         
-        return str.reverse().toString();*/
-        
-        char arr[] = new char[s.length()];
-        int j=0;
-        
-        for(int i=0; i<s.length(); i++){
-            if(s.charAt(i)=='*')
-                j--;
-            else
-                arr[j++] = s.charAt(i);
-        }
-        
-        StringBuilder str = new StringBuilder();
-        
-        for(int i=0; i<j; i++){
-            str.append(arr[i]);
-        }
-        
-        return str.toString();
+        return str.reverse().toString();
     }
 }
