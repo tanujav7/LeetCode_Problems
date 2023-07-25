b56d0bb9b653d651e511b2cc6d34f52e12498fce
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-07-25 20:20:10 +0530

    Time: 1 ms (91.70%), Space: 40.8 MB (60.72%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -1,7 +1,7 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
         
-        int count = 0;
+      /*  int count = 0;
         
         for(int i=0; i<jewels.length(); i++){
             for(int j=0; j<stones.length(); j++){
@@ -9,6 +9,21 @@ class Solution {
                     count++;
             }
         }
+        
+        return count;*/
+        
+        Set<Character> set = new HashSet<>();
+        
+        for(char x : jewels.toCharArray())
+            set.add(x);
+        
+        int count = 0;
+        
+        for(int j=0; j<stones.length(); j++){
+            if(set.contains(stones.charAt(j)))
+                count++;
+        }
+        
         
         return count;
     }
