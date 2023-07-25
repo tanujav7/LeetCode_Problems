1815369361b6f8865a0dca3677d98495611c5ea3
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-07-25 18:44:30 +0530

    Time: 3 ms (97.99%), Space: 42.3 MB (62.06%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -2,12 +2,13 @@ class Solution {
     public boolean isPalindrome(String s) {
         
         s = s.toLowerCase();
-        int i=0, j=s.length()-1;
+        int i = 0;
+        int j = s.length()-1;
         
         while(i<j){
-            while(i<j && !((s.charAt(i)>='a' && s.charAt(i)<='z') || s.charAt(i)>='0' && s.charAt(i)<='9'))
+            while(i<j && !(((s.charAt(i)>='a' && s.charAt(i)<='z') || (s.charAt(i)>='0' && s.charAt(i)<='9'))))
                 i++;
-            while(i<j && !((s.charAt(j)>='a' && s.charAt(j)<='z') || s.charAt(j)>='0' && s.charAt(j)<='9'))
+            while(i<j && !(((s.charAt(j)>='a' && s.charAt(j)<='z') || (s.charAt(j)>='0' && s.charAt(j)<='9'))))
                 j--;
             if(s.charAt(i)!=s.charAt(j))
                 return false;
