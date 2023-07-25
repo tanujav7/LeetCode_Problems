21be418faa50be1d06b708acc280cc8794974a74
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-07-25 14:18:07 +0530

    Time: 12 ms (51.72%), Space: 44.1 MB (57.37%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,40 +1,33 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        Stack<Integer> st = new Stack<Integer>();
-
-        for (int asteroid : asteroids) {
+        
+        Stack<Integer> stack = new Stack();
+        
+        for(int asteroid : asteroids){
             boolean flag = true;
-            while (!st.isEmpty() && (st.peek() > 0 && asteroid < 0)) {
-                // If the top asteroid in the stack is smaller, then it will explode.
-                // Hence pop it from the stack, also continue with the next asteroid in the stack.
-                if (Math.abs(st.peek()) < Math.abs(asteroid)) {
-                    st.pop();
+            while(!stack.isEmpty() && stack.peek()>0 && asteroid<0){
+                if(stack.peek()<Math.abs(asteroid)){
+                    stack.pop();
                     continue;
                 }
-                // If both asteroids have the same size, then both asteroids will explode.
-                // Pop the asteroid from the stack; also, we won't push the current asteroid to the stack.
-                else if (Math.abs(st.peek()) == Math.abs(asteroid)) {
-                    st.pop();
-                }
-
-                // If we reach here, the current asteroid will be destroyed
-                // Hence, we should not add it to the stack
+                else if(stack.peek()==Math.abs(asteroid))
+                    stack.pop();
                 flag = false;
                 break;
             }
-
-            if (flag) {
-                st.push(asteroid);
-            }
+            if(flag)
+            stack.push(asteroid);
         }
-
-        // Add the asteroids from the stack to the vector in the reverse order.
-        int[] remainingAsteroids = new int[st.size()];
-        for (int i = remainingAsteroids.length - 1; i >= 0; i--) {
-            remainingAsteroids[i] = st.peek();
-            st.pop();
+        
+        int res[] = new int[stack.size()];
+        int size = stack.size();
+        int k = 0;
+        
+        for(int i=size-1; i>=0; --i){
+            res[i] = stack.peek();
+            stack.pop();
         }
-
-        return remainingAsteroids;
+        
+        return res;
     }
 }
