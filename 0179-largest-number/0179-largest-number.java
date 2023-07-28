5fc90e4dfdf47197f25049d50fd5c49c814f42b8
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-07-28 13:17:53 +0530

    Time: 10 ms (13.77%), Space: 45.2 MB (6.16%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -2,16 +2,14 @@ class Solution {
     public String largestNumber(int[] nums) {
         
         int n = nums.length;
-        
         String arr[] = new String[n];
         
-        for(int i=0; i<n; i++){
+        for(int i=0; i<n; i++)
             arr[i] = String.valueOf(nums[i]);
-        }
         
         Arrays.sort(arr,(a,b)->{
-            double v1 = Double.parseDouble(a+b);
-            double v2 = Double.parseDouble(b+a);
+            Double v1 = Double.parseDouble(a+b);
+            Double v2 = Double.parseDouble(b+a);
             
             if(v1>v2)
                 return 1;
@@ -23,13 +21,13 @@ class Solution {
         
         StringBuilder str = new StringBuilder();
         
-        for(int i=arr.length-1; i>=0; --i){
+        for(int i=arr.length-1; i>=0; --i)
             str.append(arr[i]);
-        }
         
         if(str.charAt(0)=='0')
             return "0";
         
         return str.toString();
+        
     }
 }
