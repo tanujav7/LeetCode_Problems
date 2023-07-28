39313fc9b265a6ed17377686381ed9b7c434adfa
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-07-28 18:02:47 +0530

    Time: 2 ms, Memory: 40.2 MB - LeetHub

---
+++ valid-parentheses/valid-parentheses.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch=='(' || ch=='{' || ch=='[')
+                stack.push(ch);
+            else if(!stack.isEmpty()){
+                if(ch==']' && stack.peek()=='[')
+                    stack.pop();
+               else if(ch=='}' && stack.peek()=='{')
+                    stack.pop();
+               else if(ch==')' && stack.peek()=='(')
+                    stack.pop();
+                else
+                    stack.push(ch);
+            }
+            
+            else
+                stack.push(ch);
+        }
+        
+        return stack.isEmpty();
+                       
+    }
+}
