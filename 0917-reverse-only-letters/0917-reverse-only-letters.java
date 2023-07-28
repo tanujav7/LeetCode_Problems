25a1f4af635357cc2a5b0508a2e85e5e52476e32
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-07-28 12:34:39 +0530

    Time: 1 ms (47.15%), Space: 40.6 MB (79.55%) - LeetHub

--- 0917-reverse-only-letters/0917-reverse-only-letters.java
+++ 0917-reverse-only-letters/0917-reverse-only-letters.java
@@ -1,26 +1,20 @@
 class Solution {
     public String reverseOnlyLetters(String s) {
         
-        Stack<Character> stack = new Stack<>();
-        int n = s.length();
-        
-        for(int i=0; i<n; i++){
-            char ch = s.charAt(i);
-            if(Character.isLetter(ch))
-                stack.push(ch);
-        }
-        
         StringBuilder str = new StringBuilder();
+        int j = s.length()-1;
         
-        for(int i=0; i<n; i++){
-            char ch = s.charAt(i);
-            if(Character.isLetter(ch))
-                str.append(stack.pop());
-            else
-                str.append(ch);
+        for(int i=0; i<s.length(); i++){
+            if(Character.isLetter(s.charAt(i))){
+                while(!Character.isLetter(s.charAt(j)))
+                    j--;
+                str.append(s.charAt(j--));
+            }
+            else{
+                str.append(s.charAt(i));
+            }
         }
         
         return str.toString();
-        
     }
 }
