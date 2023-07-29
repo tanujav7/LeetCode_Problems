e8e7366dd12496032a1d12158a202c6eaf2b051e
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-07-29 20:34:51 +0530

    Time: 12 ms (60.78%), Space: 44.6 MB (8.04%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -6,17 +6,17 @@ class Solution {
         for(int i=0; i<magazine.length(); i++){
             char ch = magazine.charAt(i);
             if(map.containsKey(ch))
-                map.put(ch, map.get(ch)+1);
+                map.put(ch,map.get(ch)+1);
             else
                 map.put(ch,1);
         }
         
         for(int i=0; i<ransomNote.length(); i++){
             char ch = ransomNote.charAt(i);
-          int count = map.containsKey(ch) ? map.get(ch)-1 : -1;
-          if(count==-1)
-              return false; 
-          map.put(ch,count);
+            int count = (map.containsKey(ch)) ? map.get(ch)-1 : -1;
+            if(count==-1)
+                return false;
+            map.put(ch,count);
         }
         
         return true;
