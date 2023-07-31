47fe4de5f40bd3b3c0888f1511048010bd88e239
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-08-01 00:08:31 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (97.88%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -9,10 +9,8 @@ class Solution {
             
             if(nums[mid]==target)
                 return mid;
-            
             else if(nums[mid]<target)
                 left = mid+1;
-            
             else
                 right = mid-1;
         }
