504ac3577bd6fd7e6415a2a43ee43cc8b8ef060f
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-08-04 00:10:44 +0530

    Time: 3 ms (75.55%), Space: 46 MB (94.31%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -1,10 +1,9 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
         
-        int left = 0;
         int zeroCount = 0;
+        int left = 0;
         int longestOnes = 0;
-        
         for(int right=0; right<nums.length; right++){
             zeroCount += (nums[right]==0) ? 1 : 0;
             
@@ -13,7 +12,7 @@ class Solution {
                 left++;
             }
             
-            longestOnes = Math.max(longestOnes, right-left+1);
+            longestOnes = Math.max(longestOnes,right-left+1);
         }
         
         return longestOnes;
