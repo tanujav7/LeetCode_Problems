0a19ed9f841ad19eace0e32059eb36ca5b2eedfc
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-08-06 19:17:32 +0530

    Time: 2 ms (86.90%), Space: 43 MB (80.50%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.java
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.java
@@ -3,16 +3,14 @@ class Solution {
         
         Stack<Integer> stack = new Stack<>();
         int j = 0;
-        int n = pushed.length;
         
         for(int x : pushed){
             stack.push(x);
             
-            while(!stack.isEmpty() && j<n && stack.peek()==popped[j]){
+            while(!stack.isEmpty() && stack.peek()==popped[j]){
                 stack.pop();
                 j++;
             }
-        
         }
         
         return stack.isEmpty();
