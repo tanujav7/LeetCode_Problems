f531115fb88bd167b8c8627fd26afa80c1954a97
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-08-09 20:24:53 +0530

    Time: 4 ms (77.71%), Space: 41.7 MB (99.91%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,29 +1,27 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         
-        int n1 = nums1.length;
-        int n2 = nums2.length;
-        int res[] = new int[n1];
+        int n = nums1.length;
         
-        Map<Integer, Integer> map = new HashMap<>();
+        int res[] = new int[n];
         
-        for(int i=0; i<n2; i++)
-            map.put(nums2[i],i);
+        int m = nums2.length;
         
-        int k = 0;
-        int j = 0;
-        for(int i=0; i<n1; i++){
-            for(j=map.get(nums1[i])+1; j<n2; j++){
-                if(nums1[i]<nums2[j]){
-                    res[k++] = nums2[j];
+        for(int i=0; i<n; i++){
+            boolean found = false;
+            boolean flag = false;
+            for(int j=0; j<m; j++){
+                if(nums1[i]==nums2[j])
+                    found = true;
+                if(found==true && nums2[j]>nums1[i]){
+                    res[i] = nums2[j];
+                    flag = true;
                     break;
                 }
             }
-            
-            if(j==n2)
-                res[k++] = -1;
+            if(flag==false)
+            res[i] = -1;
         }
-        
         
         return res;
     }
