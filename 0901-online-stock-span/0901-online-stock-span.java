722075a69dbaf55c71aef33c8e5e9b2fd87ebab2
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-08-09 19:45:58 +0530

    Time: 28 ms (90.53%), Space: 54.8 MB (72.97%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.java
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -9,10 +9,11 @@ class StockSpanner {
     public int next(int price) {
         int res = 1;
         
-        while(!stack.isEmpty() && price>=stack.peek()[0])
-            res += stack.pop()[1];
+        while(!stack.isEmpty() && stack.peek()[0]<=price)
+            res = res + stack.pop()[1];
         
         stack.push(new int[] {price,res});
+        
         return res;
     }
 }
