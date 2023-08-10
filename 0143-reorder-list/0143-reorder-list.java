bbb8211474ed90da615d29d011a432ecf56b7446
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-08-10 14:20:31 +0530

    Time: 2 ms (45.42%), Space: 45.7 MB (92.87%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -11,12 +11,10 @@
 class Solution {
     public void reorderList(ListNode head) {
         
-        ListNode middleNode = middleOfTheList(head);
-        ListNode middleNext = middleNode.next;
-        middleNode.next = null;
-        
         ListNode p1 = head;
-        ListNode p2 = reverseList(middleNext);
+        ListNode middleNode = middleOfTheList(head);
+        ListNode p2 = reverseList(middleNode.next);
+        middleNode.next = null;
         
         ListNode dummyHead = new ListNode(0);
         ListNode currentNode = dummyHead;
@@ -27,16 +25,17 @@ class Solution {
                 p1 = p1.next;
                 currentNode = currentNode.next;
             }
-            
-             if(p2!=null){
+            if(p2!=null){
                 currentNode.next = p2;
                 p2 = p2.next;
                 currentNode = currentNode.next;
             }
         }
+        currentNode.next = null;
         
         head = dummyHead.next;
     }
+    
     
     ListNode middleOfTheList(ListNode head){
         ListNode slow = head, fast = head;
@@ -50,7 +49,7 @@ class Solution {
     }
     
     ListNode reverseList(ListNode head){
-        ListNode currentNode = head, prevNode = null, nextNode = null;
+        ListNode currentNode = head, nextNode = null, prevNode = null;
         
         while(currentNode!=null){
             nextNode = currentNode.next;
