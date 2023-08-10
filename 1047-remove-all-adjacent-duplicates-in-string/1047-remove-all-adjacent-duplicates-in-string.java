dd8b7b8aacefd6bfb6693747cf69bdbc29cb4e41
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-08-10 19:16:04 +0530

    Time: 36 ms (67.46%), Space: 44.3 MB (74.64%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -5,20 +5,22 @@ class Solution {
         
         for(char ch : s.toCharArray()){
             boolean flag = true;
+            
             while(!stack.isEmpty() && stack.peek()==ch){
                 stack.pop();
                 flag = false;
             }
+            
             if(flag)
                 stack.push(ch);
         }
         
         StringBuilder sb = new StringBuilder();
         
-        while(!stack.isEmpty()){
+        while(!stack.isEmpty())
             sb.append(stack.pop());
-        }
         
         return sb.reverse().toString();
+        
     }
 }
