7ca245bc5730316545cb9291d45c2fbf4277bd36
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-08-13 10:13:33 +0530

    Time: 3 ms (48.32%), Space: 45.1 MB (26.34%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -70,7 +70,7 @@ class Solution {
         
         //Approach 4 - Using Extra Array
         
-        int n = nums.length;
+      /*  int n = nums.length;
         int arr[] = new int[n+1];
         int duplicate = -1; 
         int missing = 1;
@@ -83,6 +83,29 @@ class Solution {
                 missing = i;
             else if(arr[i]==2)
                 duplicate = i;
+        }
+        
+        return new int[]{duplicate, missing};
+        
+        */
+        // Approach - 5 -> Using Constant Space
+        
+          int duplicate = -1, missing = 1;
+        
+          int len = nums.length;
+        
+          for(int n : nums){
+              
+              if(nums[Math.abs(n)-1]<0)
+                  duplicate = Math.abs(n);
+              else
+                  nums[Math.abs(n)-1] = -1 * nums[Math.abs(n)-1];
+              
+          }
+        
+        for(int i=0; i<len; i++){
+            if(nums[i]>0)
+                missing = i+1;
         }
         
         return new int[]{duplicate, missing};
