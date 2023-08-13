0f74b1628b6f492f870f7242c17c6614dbe48720
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-08-13 09:49:19 +0530

    Time: 696 ms (6.50%), Space: 45.2 MB (14.32%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,12 +1,13 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        // Approach 1
+        // Approach 1 - Brute Force
         
-        int duplicate = -1; 
+      /*  int duplicate = -1; 
         int missing = -1;
         int n = nums.length;
         
         for(int i=1; i<=n; i++){
+            
             int count = 0;      
             for(int j=0; j<n; j++){
                 if(i==nums[j])
@@ -21,6 +22,35 @@ class Solution {
             
         }
         
+        return new int[]{duplicate, missing};*/
+        
+        
+        //Approach 2 - Better Brute Force
+        
+        int duplicate = -1; 
+        int missing = -1;
+        int n = nums.length;
+        
+        for(int i=1; i<=n; i++){
+            
+            int count = 0;      
+            for(int j=0; j<n; j++){
+                if(i==nums[j])
+                    count++;
+            }
+            
+            if(count==0)
+                missing = i;
+            
+            if(count==2)
+                duplicate = i;
+            
+            if(missing>0 && duplicate>0)
+                break;
+        }
+        
         return new int[]{duplicate, missing};
+        
+        
     }
 }
