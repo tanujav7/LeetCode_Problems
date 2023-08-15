b414b98506d8b6d6126ad846c41415e3ea556d8f
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-08-15 12:52:02 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (95.31%) - LeetHub

--- 0086-partition-list/0086-partition-list.java
+++ 0086-partition-list/0086-partition-list.java
@@ -24,15 +24,18 @@ class Solution {
                 small.next = new ListNode(tempNode.val);
                 small = small.next;
             }
+            
             else{
                 high.next = new ListNode(tempNode.val);
                 high = high.next;
-            } 
+            }
+            
             tempNode = tempNode.next;
         }
         
         small.next = higherHead.next;
         
         return smallerHead.next;
+        
     }
 }
