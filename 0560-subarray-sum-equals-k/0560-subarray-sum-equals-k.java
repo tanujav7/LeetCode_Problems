2f549a9658b1f12cce14365144655efbe3808e57
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-08-24 12:13:21 +0530

    Time: 23 ms (83.09%), Space: 45.7 MB (64.76%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -5,17 +5,17 @@ class Solution {
         
         map.put(0,1);
         
-        int sum = 0, count = 0;
+        int sum = 0;
+        
+        int count = 0;
         
         for(int i=0; i<nums.length; i++){
-            
             sum = sum + nums[i];
             
             if(map.containsKey(sum-k))
                 count = count + map.get(sum-k);
             
             map.put(sum, map.getOrDefault(sum, 0)+1);
-            
         }
         
         return count;
