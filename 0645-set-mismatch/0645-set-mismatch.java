3f94a69dec8e9b7104a33f7b06b69a8fb7cf7e3e
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-08-24 12:47:44 +0530

    Time: 697 ms (5.80%), Space: 44.7 MB (76.49%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,14 +1,12 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        // Approach 1 - Brute Force
         
-      /*  int duplicate = 0; 
-        int missing = 0;
+        int duplicate = 0, missing = 0;
+        
         int n = nums.length;
         
         for(int i=1; i<=n; i++){
-            
-            int count = 0;      
+            int count = 0;
             for(int j=0; j<n; j++){
                 if(i==nums[j])
                     count++;
@@ -17,119 +15,13 @@ class Solution {
             if(count==0)
                 missing = i;
             
-            if(count==2)
+            else if(count==2)
                 duplicate = i;
             
-        }
-        
-        return new int[]{duplicate, missing};
-        */
-       
-        //Approach 2 - Better Brute Force
-        
-      /*  int duplicate = 0; 
-        int missing = 0;
-        int n = nums.length;
-        
-        for(int i=1; i<=n; i++){
-            
-            int count = 0;      
-            for(int j=0; j<n; j++){
-                if(i==nums[j])
-                    count++;
-            }
-            
-            if(count==0)
-                missing = i;
-            
-            if(count==2)
-                duplicate = i;
-            
-            if(missing>0 && duplicate>0)
+            else if(missing>0 && duplicate>0)
                 break;
         }
         
         return new int[]{duplicate, missing};
-        
-*/
-        
-        //Approach 3 - Sorting
-        
-    /*     Arrays.sort(nums);
-        
-        int duplicate = 0, missing = 1;
-        int n = nums.length;
-        
-        for (int i = 1; i < n; i++) {
-            
-            if (nums[i] == nums[i - 1])
-                duplicate = nums[i];
-            else if (nums[i] > nums[i - 1] + 1)
-                missing = nums[i - 1] + 1;
-            
-        }
-        
-        return new int[] {duplicate, nums[n - 1] != n ? n : missing};
-        
-*/
-        
-        
-        //Approach 4 - Using Extra Array
-       /* 
-        int n = nums.length;
-        int arr[] = new int[n+1];
-        int duplicate = 0; 
-        int missing = 0;
-        
-        for(int i=0; i<n; i++)
-            arr[nums[i]]++;
-        
-        for(int i=1; i<arr.length; i++){
-            if(arr[i]==0)
-                missing = i;
-            else if(arr[i]==2)
-                duplicate = i;
-        }
-        
-        return new int[]{duplicate, missing};
-    
-        */
-        // Approach - 5 -> Using Constant Space
-        
-          int duplicate = 0, missing = 0;
-        
-          int len = nums.length;
-        
-          for(int n : nums){
-              
-              if(nums[Math.abs(n)-1]<0)
-                  duplicate = Math.abs(n);
-              else
-                  nums[Math.abs(n)-1] = -1 * nums[Math.abs(n)-1];
-              
-          }
-        
-        for(int i=0; i<len; i++){
-            if(nums[i]>0)
-                missing = i+1;
-        }
-        
-        return new int[]{duplicate, missing};
-        
-      //  Approach 6: Using Map
-        
-     /*   Map < Integer, Integer > map = new HashMap();
-        int duplicate = 0, missing = 0;
-        for (int n: nums) {
-            map.put(n, map.getOrDefault(n, 0) + 1);
-        }
-        for (int i = 1; i <= nums.length; i++) {
-            if (map.containsKey(i)) {
-                if (map.get(i) == 2)
-                    duplicate = i;
-            } else
-                missing = i;
-        }
-        return new int[]{duplicate, missing};*/
     }
 }
