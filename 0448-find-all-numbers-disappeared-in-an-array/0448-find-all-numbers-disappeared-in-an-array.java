77ad9ce3c1588861387236a284142fa5df7a1085
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-08-24 11:21:22 +0530

    Time: 6 ms (72.04%), Space: 53.6 MB (50.08%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -3,16 +3,16 @@ class Solution {
         
         List<Integer> list = new ArrayList<>();
         
-        Set<Integer> set = new HashSet<>();
-        
         int n = nums.length;
         
-        for(int num : nums)
-            set.add(num);
+        for(int i=0; i<n; i++){
+            int index = Math.abs(nums[i])-1;
+            nums[index] = -1 * Math.abs(nums[index]);
+        }
         
-        for(int i=1; i<=n; i++){
-            if(!set.contains(i))
-                list.add(i);
+        for(int i=0; i<n; i++){
+            if(nums[i]>0)
+                list.add(i+1);
         }
         
         return list;
