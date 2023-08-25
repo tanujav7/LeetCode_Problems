9afd76510bc83836b05667106aa6153120f97d9f
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-08-25 11:24:33 +0530

    Time: 1 ms (100.00%), Space: 45.1 MB (17.68%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,26 +1,19 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
         
+        int n = nums.length;
+        int arr[] = new int[n+1];
         int duplicate = 0, missing = 0;
         
-        int n = nums.length;
+        for(int i=0; i<n; i++)
+            arr[nums[i]]++;
         
-        for(int i=1; i<=n; i++){
-            int count = 0;
-            for(int j=0; j<n; j++){
-                if(i==nums[j]){
-                    count++;
-                }
-            }
-            
-            if(count==0)
+        for(int i=1; i<arr.length; i++){
+            if(arr[i]==0)
                 missing = i;
             
-            else if(count==2)
+            else if(arr[i]==2)
                 duplicate = i;
-            
-            if(missing>0 && duplicate>0)
-                break;
         }
         
         return new int[]{duplicate, missing};
