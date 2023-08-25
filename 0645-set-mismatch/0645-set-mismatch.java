a4903caa755da33421b1994ea610e793f19ca181
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-08-25 11:15:36 +0530

    Time: 730 ms (6.03%), Space: 44.6 MB (86.83%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -8,8 +8,9 @@ class Solution {
         for(int i=1; i<=n; i++){
             int count = 0;
             for(int j=0; j<n; j++){
-                if(i==nums[j])
+                if(i==nums[j]){
                     count++;
+                }
             }
             
             if(count==0)
@@ -18,7 +19,7 @@ class Solution {
             else if(count==2)
                 duplicate = i;
             
-            else if(missing>0 && duplicate>0)
+            if(missing>0 && duplicate>0)
                 break;
         }
         
