e1ed3b165510759a677470c5eae9a184c340ec03
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-08-27 20:00:22 +0530

    Time: 0 ms (100.00%), Space: 51.3 MB (8.28%) - LeetHub

---
+++ 0275-h-index-ii/0275-h-index-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int left = 0;
+        int right = n-1;
+        
+        while(left<=right){
+            int mid = left+(right-left)/2;
+            
+            if(citations[mid]<n-mid)
+                left = mid+1;
+            
+            else
+                right = mid-1;
+        }
+        
+        return n-left;
+    }
+}
