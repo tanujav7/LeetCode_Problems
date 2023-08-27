d01709875936bb5fe8eed092be051af2c78cd322
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-08-27 10:27:08 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (13.80%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -2,14 +2,12 @@ class Solution {
     public int search(int[] nums, int target) {
         
         int n = nums.length;
-        
         int res = findKey(nums, 0, n-1, target);
         
         return res;
     }
     
     int binarySearch(int arr[], int low, int high, int target){
-        
         if(low>high)
             return -1;
         
@@ -22,9 +20,8 @@ class Solution {
             return binarySearch(arr, mid+1, high, target);
         
         else
-             return binarySearch(arr, low, mid-1, target);
-        
-     } 
+            return binarySearch(arr, low, mid-1, target);
+    }
     
     int findPivot(int arr[], int start, int end){
         if(start>end)
@@ -45,22 +42,22 @@ class Solution {
             return findPivot(arr, start, mid-1);
     }
     
-    int findKey(int arr[], int low, int high, int target){
-        if(low>high)
+    int findKey(int arr[], int start, int end, int target){
+        if(start>end)
             return -1;
         
-        int pivot = findPivot(arr, low, high);
+        int pivot = findPivot(arr, start, end);
         
         if(pivot==-1)
-            return binarySearch(arr, low, high, target);
+            return binarySearch(arr, start, end, target);
         
         else if(arr[pivot]==target)
             return pivot;
         
         else if(arr[0]<=target)
-            return binarySearch(arr, low, pivot-1, target);
+            return binarySearch(arr, start, pivot-1, target);
         
         else
-             return binarySearch(arr, pivot+1, high, target);
+            return binarySearch(arr, pivot+1, end, target);
     }
 }
