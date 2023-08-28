1114990eb6e613327840f9066b7875c64c8b78cb
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-08-28 09:21:20 +0530

    Time: 1 ms (85.08%), Space: 39.8 MB (61.80%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.java
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.java
@@ -4,12 +4,14 @@ class Solution {
         int n = nums.length;
         int count = 0;
         
+        Map<Integer, Integer> map = new HashMap<>();
+        
         for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                if(nums[i]==nums[j])
-                    count++;
-            }
+            if(map.containsKey(nums[i]))
+                count = count + map.get(nums[i]);
+            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
         }
+        
         
         return count;
     }
