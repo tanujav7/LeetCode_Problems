6f6bdd6ce917580006d5cd3df7e13ecc246bc412
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-08-29 13:46:34 +0530

    Time: 23 ms (89.25%), Space: 88.9 MB (83.98%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
@@ -27,7 +27,7 @@ class RandomizedSet {
         if(!map.containsKey(val))
             return false;
         
-      if(map.get(val) != list.size()-1){  
+     // if(map.get(val) != list.size()-1){  
         int index = map.get(val);
         
         //To get the last element
@@ -38,7 +38,7 @@ class RandomizedSet {
         
         list.set(index, lastElement);
         
-      }
+   //   }
         
         map.remove(val);
         
