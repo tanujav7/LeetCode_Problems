2867577098fa27df2ed47ae69b5967e804e48dbf
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-08-29 17:43:55 +0530

    Time: 2 ms (99.73%), Space: 42.1 MB (95.77%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -3,21 +3,22 @@ class Solution {
         
         int chars[] = new int[128];
         
-        int i = 0, j = 0;
+        int n = s.length();
+        
+        int i=0, j=0;
         
         int res = 0;
         
-        while(j<s.length()){
-        
-        char right_char = s.charAt(j);
-        chars[right_char]++;
+        while(j<n){
+            char right_char = s.charAt(j);
+            chars[right_char]++;
             
-        while(chars[right_char]>1){
-            char left_char = s.charAt(i);
-            chars[left_char]--;
-            i++;
-        }
-        
+            while(chars[right_char]>1){
+                char left_char = s.charAt(i);
+                chars[left_char]--;
+                i++;
+            }
+            
             res = Math.max(res, j-i+1);
             j++;
         }
