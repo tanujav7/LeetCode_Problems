2cc2df705167c3d8f0e4d479adfaf40ed2950353
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-09-21 21:02:19 +0530

    Time: 2 ms (72.48%), Space: 44.6 MB (68.51%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,34 +1,45 @@
 class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-      
-        int m = nums1.length;
-        int n = nums2.length;
+    public double findMedianSortedArrays(int[] a, int[] b) {
+        int n1 = a.length;
+        int n2 = b.length;
+        int n = n1+n2;
+        int left = (n1+n2+1)/2;
+        //To find the length of the nums2 mid
         
-        int res[] = new int[m+n];
+        if(n1>n2)
+            return findMedianSortedArrays(b, a);
         
-        int k = 0;
-        int i=0, j=0;
-        
-        while(i<m && j<n){
-            if(nums1[i]<nums2[j])
-                res[k++] = nums1[i++];
+        int low = 0, high = n1;
+         double res = 0;
+        while(low<=high){
+            int mid1 = low+(high-low)/2;
             
+            int mid2 = left-mid1;
+            
+            //To calculate the left and right sides of nums1, num2
+            int l1 = (mid1>0) ? a[mid1-1] : Integer.MIN_VALUE;
+            int l2 = (mid2>0) ? b[mid2-1] : Integer.MIN_VALUE;
+            int r1 = (mid1<n1) ? a[mid1] : Integer.MAX_VALUE;
+            int r2 = (mid2<n2) ? b[mid2] : Integer.MAX_VALUE;
+            
+            if(l1<=r2 && l2<=r1){
+                if(n%2==0){
+                    int l = Math.max(l1, l2);
+                    int r = Math.min(r1, r2);
+                    return (l+r)/2.0;
+                }
+                
+                else{
+                    return Math.max(l1, l2);
+                }
+            }
+            
+            else if(l1 > r2)
+                high = mid1-1;
             else
-                res[k++] = nums2[j++];
+                low = mid1+1;
         }
         
-        while(i<m)
-             res[k++] = nums1[i++];
-        
-        while(j<n)
-             res[k++] = nums2[j++];
-        
-        
-      if(k%2==1)
-          return (double)(res[k/2]);
-        
-      else
-          return (double)(res[k/2]+(double)res[(k/2)-1])/2;
-        
+        return 0;
     }
 }
