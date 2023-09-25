aa3e04b38ab3f32a667f274d1ef35210500a5cba
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-09-25 19:36:25 +0530

    Time: 1 ms (100.00%), Space: 40.9 MB (29.39%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,15 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        
+        int[] count = new int[128];
+
+      for (final char c : s.toCharArray())
+      ++count[c];
+
+    for (final char c : t.toCharArray())
+      if (--count[c] < 0)
+        return c;
+
+        return ' ';
+    }
+}
