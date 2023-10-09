ebca0ba687071c5207dfced9535e58b2fba68ade
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-10-09 19:57:01 +0530

    Time: 0 ms (100.00%), Space: 44.4 MB (38.52%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,51 +1,61 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
+                int res[] = {-1,-1};
         
-        int first = firstOccurrence(nums, target);
-        int last = lastOccurrence(nums, target);
+         res[0] = firstOcurrence(nums,target);
+         res[1] = lastOcurrence(nums,target);
         
-        return new int[]{first, last};
+        return res;
     }
     
-    int firstOccurrence(int arr[], int target){
+    int firstOcurrence(int arr[], int target){
+        
         int low = 0;
         int high = arr.length-1;
         int res = -1;
+        
         while(low<=high){
+            
             int mid = low+(high-low)/2;
             
             if(arr[mid]==target){
                 res = mid;
                 high = mid-1;
             }
-            
+           
             else if(arr[mid]<target)
                 low = mid+1;
             
             else
                 high = mid-1;
+           
         }
         
         return res;
     }
     
-     int lastOccurrence(int arr[], int target){
+    
+    int lastOcurrence(int arr[], int target){
+        
         int low = 0;
         int high = arr.length-1;
         int res = -1;
+        
         while(low<=high){
+            
             int mid = low+(high-low)/2;
             
             if(arr[mid]==target){
                 res = mid;
                 low = mid+1;
             }
-            
+           
             else if(arr[mid]<target)
                 low = mid+1;
             
             else
                 high = mid-1;
+           
         }
         
         return res;
