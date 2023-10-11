ff331ba11b480f9d40f7673e38cf1832bc891f0b
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-10-11 21:15:42 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (84.42%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.java
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -15,25 +15,23 @@
  */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        
         invert(root);
         return root;
-        
     }
     
-    void invert(TreeNode node){
+     void invert(TreeNode node){
         if(node==null)
             return;
-        
-        swap(node);
-        invert(node.left);
-        invert(node.right);
-        
-    }
+         
+         swap(node);
+         
+         invert(node.left);
+         invert(node.right);
+     }
     
-    void swap(TreeNode node){
-        TreeNode tempNode = node.left;
+      void swap(TreeNode node){
+        TreeNode temp = node.left;
         node.left = node.right;
-        node.right = tempNode;
+        node.right = temp;
     }
 }
