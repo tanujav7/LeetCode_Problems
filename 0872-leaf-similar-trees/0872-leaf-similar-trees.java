62ba65721ab0443bf068dcfe32acace2b8eb444a
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-10-13 21:37:12 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (23.24%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.java
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.java
@@ -16,22 +16,24 @@
 class Solution {
     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
         
-        List<Integer> leafNodes1 = new ArrayList<>();
-        List<Integer> leafNodes2 = new ArrayList<>();
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
         
-        getLeafNodes(root1, leafNodes1);
-        getLeafNodes(root2, leafNodes2);
+         getLeafNodes(root1, list1);
+         getLeafNodes(root2, list2);
         
-        return (leafNodes1.equals(leafNodes2));
-        
+        return list1.equals(list2);
     }
     
-    void getLeafNodes(TreeNode node, List<Integer> leafNodes){
-        if(node!=null){
-            if(node.left==null && node.right==null)
-                leafNodes.add(node.val);
-            getLeafNodes(node.left, leafNodes);
-            getLeafNodes(node.right, leafNodes);
-        }
+    void getLeafNodes(TreeNode node, List<Integer>list){
+        if(node==null)
+            return;
+        
+        if(node.left==null && node.right==null)
+            list.add(node.val);
+        
+        getLeafNodes(node.left, list);
+        getLeafNodes(node.right, list);
+        
     }
 }
