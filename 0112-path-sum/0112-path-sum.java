7695badb13b9c76220a523e0349d0bc09963c2d9
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-10-15 16:18:50 +0530

    Time: 0 ms (100.00%), Space: 43 MB (85.38%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -15,13 +15,12 @@
  */
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        if(root==null)
-            return false;
-
         
-        return findSumPath(root, 0, targetSum);
+        return checkPathSum(root, targetSum, 0);
+        
     }
-    boolean findSumPath(TreeNode node, int currentSum, int targetSum){
+    
+    boolean checkPathSum(TreeNode node, int targetSum, int currentSum){
         
         if(node==null)
             return false;
@@ -29,9 +28,9 @@ class Solution {
         currentSum += node.val;
         
         if(node.left==null && node.right==null && currentSum==targetSum)
-         return true;
-      
+            return true;
         
-        return (findSumPath(node.left, currentSum, targetSum) || findSumPath(node.right, currentSum, targetSum));
+        return (checkPathSum(node.left, targetSum, currentSum) || checkPathSum(node.right, targetSum, currentSum));
+        
     }
 }
