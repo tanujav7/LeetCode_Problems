7e306ec59fc83c6266d91b48d8f24cab64c63eaf
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-10-30 16:55:51 +0530

    Time: 1 ms (100.00%), Space: 60 MB (11.03%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,21 +1,16 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-      //Kadane's Algorithm
         
-        int maximum_ending_here = 0;
-        int maximum_so_far = Integer.MIN_VALUE;
+        int maxSum = Integer.MIN_VALUE;
+        int sum = 0;
         
         for(int i=0; i<nums.length; i++){
-
-            maximum_ending_here = maximum_ending_here + nums[i];
-            
-            if(maximum_ending_here<nums[i])
-                maximum_ending_here = nums[i];
-            
-            if(maximum_ending_here>maximum_so_far)
-                maximum_so_far = maximum_ending_here;
+            sum = sum + nums[i];
+            if(nums[i]>sum)
+                sum = nums[i];
+            maxSum = Math.max(maxSum, sum);
         }
         
-        return maximum_so_far;
+        return maxSum;
     }
 }
