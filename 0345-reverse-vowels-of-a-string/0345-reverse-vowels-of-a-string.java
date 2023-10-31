bd73ab55225328236013075ba261651a63d5be3a
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-10-31 19:46:45 +0530

    Time: 3 ms (74.07%), Space: 43.7 MB (62.37%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -2,39 +2,38 @@ class Solution {
     public String reverseVowels(String s) {
         
         Set<Character> set = new HashSet<>();
+        set.add('a');
+        set.add('e');
+        set.add('i');
+        set.add('o');
+        set.add('u');
+        set.add('A');
+        set.add('E');
+        set.add('I');
+        set.add('O');
+        set.add('U');
         
-         set.add('a');
-         set.add('e');
-         set.add('i');
-         set.add('o');
-         set.add('u');
-         set.add('A');
-         set.add('E');
-         set.add('I');
-         set.add('O');
-         set.add('U');
-        
-         char arr[] = s.toCharArray();
-        
-         int i = 0;
-         int j = s.length()-1;
+        char arr[] = s.toCharArray();
+        int i = 0, j = arr.length-1;
         
         while(i<j){
-           if(!set.contains(arr[i]))
-                i++;
-            else if(!set.contains(arr[j]))
-                j--;
-            else{
-                char temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-                i++;
-                j--;
-            }
-        }
+            
+         while(i<j && !set.contains(arr[i]))
+             i++;
+            
+         while(i<j &&!set.contains(arr[j]))
+             j--;
+            
+             char temp = arr[i];
+             arr[i] = arr[j];
+             arr[j] = temp;
+         
+             i++;
+             j--;
+          
+       }
         
-        String str = new String(arr);
-        
-        return str;
+       String str = new String(arr);
+       return str;
     }
 }
