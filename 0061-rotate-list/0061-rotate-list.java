ee03503268e38eaa19e202890b713536c6f2ecdb
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-11-02 13:21:30 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (6.05%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -15,32 +15,34 @@ class Solution {
             return null;
         
         int length = 0;
-        ListNode tempNode = head;
+        ListNode currentNode = head;
         
-        while(tempNode!=null){
-            tempNode = tempNode.next;
+        while(currentNode!=null){
             length++;
+            currentNode = currentNode.next;
         }
         
         k = k%length;
         
         ListNode slow = head, fast = head;
         
-        
         for(int i=0; i<k; i++)
             fast = fast.next;
         
+   //     ListNode res = fast.next;
+        
         while(fast.next!=null){
-            slow = slow.next;
-            fast = fast.next;
+             slow = slow.next;
+             fast = fast.next;
         }
         
         fast.next = head;
-        
         ListNode res = slow.next;
-        
         slow.next = null;
         
         return res;
+       
+        
+        
     }
 }
