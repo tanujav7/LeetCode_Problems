76caab793a1d8a1100f62730b89a282826c852c0
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-11-18 11:33:47 +0530

    Time: 1 ms (89.94%), Space: 41.1 MB (58.71%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -21,29 +21,35 @@ class Solution {
         
         queue.add(root);
         
+        int level = 1;
+        
         if(root==null)
             return resList;
-        int level = 1;
         
         while(!queue.isEmpty()){
             int size = queue.size();
             List<Integer> list = new ArrayList<>();
             for(int i=0; i<size; i++){
                 TreeNode temp = queue.remove();
-                if(level%2!=0)
-                    list.add(temp.val);
-                else
+                
+                if(level%2==0)
                     list.add(0, temp.val);
+                
+                else
+                    list.add(temp.val);
                 
                 if(temp.left!=null)
                     queue.add(temp.left);
+                
                  if(temp.right!=null)
                     queue.add(temp.right);
             }
+            
             resList.add(list);
             level++;
         }
         
         return resList;
+        
     }
 }
