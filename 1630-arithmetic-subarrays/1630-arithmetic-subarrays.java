d00066e0e601914a0957708f52d0bfdeb5f1ef7a
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-11-23 19:39:45 +0530

    Time: 17 ms (77.20%), Space: 44.4 MB (38.19%) - LeetHub

---
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.java
@@ -0,0 +1,28 @@
+class Solution {
+    public Boolean check(int[] arr) {
+        Arrays.sort(arr);
+        int diff = arr[1] - arr[0];
+        
+        for (int i = 2; i < arr.length; i++) {
+            if (arr[i] - arr[i - 1] != diff) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    public List<Boolean> checkArithmeticSubarrays(int[] nums, int[] l, int[] r) {
+        List<Boolean> ans = new ArrayList();
+        for (int i = 0; i < l.length; i++) {
+            int[] arr = new int[r[i] - l[i] + 1];
+            for (int j = 0; j < arr.length; j++) {
+                arr[j] = nums[l[i] + j];
+            }
+            
+            ans.add(check(arr));
+        }
+
+        return ans;
+    }
+}
