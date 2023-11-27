923a965d2bad8e24003aa55ed34038a3c8139b5f
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-11-27 19:27:57 +0530

    Time: 1 ms (71.06%), Space: 42.2 MB (9.31%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,14 +1,11 @@
 class Solution {
-    
     List<List<Integer>> resList = new ArrayList<>();
-    
     public List<List<Integer>> subsets(int[] nums) {
-        backTrack(nums, 0, new ArrayList<>());
+        backtrack(nums, 0, new ArrayList<>());
         return resList;
     }
     
-    void backTrack(int nums[], int index, List<Integer> list){
-        
+    void backtrack(int nums[], int index, List<Integer> list){
         if(index==nums.length){
             resList.add(new ArrayList<>(list));
             return;
@@ -16,9 +13,9 @@ class Solution {
         
         else{
             list.add(nums[index]);
-            backTrack(nums, index+1, list);
+            backtrack(nums, index+1, list);
             list.remove(list.size()-1);
-            backTrack(nums, index+1, list);
+            backtrack(nums, index+1, list);
         }
     }
 }
