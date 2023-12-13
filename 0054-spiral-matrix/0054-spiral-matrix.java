72f47594460c4ab3a0b1b975d3d80d04fb1a7e51
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-12-13 12:25:25 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (60.92%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,41 +1,40 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-       
         
-      List<Integer> res = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
         
         int row_Begin = 0;
-        int row_End = matrix.length-1;
         int col_Begin = 0;
+        
+        int row_End = matrix.length-1;
         int col_End = matrix[0].length-1;
         
+        
         while(row_Begin<=row_End && col_Begin<=col_End){
-
-        for(int i=col_Begin; i<=col_End; i++)
-            res.add(matrix[row_Begin][i]);
+            for(int i=col_Begin; i<=col_End; i++)
+                list.add(matrix[row_Begin][i]);
+            row_Begin++;
             
-        row_Begin++;
+            for(int i=row_Begin; i<=row_End; i++)
+                list.add(matrix[i][col_End]);
+            col_End--;
             
+           
+            if(row_Begin<=row_End){
+                for(int i=col_End; i>=col_Begin; --i)
+                list.add(matrix[row_End][i]);
+             
+            }
+             row_End--;
             
-        for(int i=row_Begin; i<=row_End; i++)
-            res.add(matrix[i][col_End]);
-            
-        col_End--;
-            
-        if(row_Begin<=row_End){
-            for(int i=col_End; i>=col_Begin; --i)
-                res.add(matrix[row_End][i]);
-        }
-            row_End--;
-            
-        if(col_Begin<=col_End){
-            for(int i=row_End; i>=row_Begin; --i)
-                res.add(matrix[i][col_Begin]);
-        }
-        
-            col_Begin++;
+               if(col_Begin<=col_End){
+                for(int i=row_End; i>=row_Begin; --i)
+                list.add(matrix[i][col_Begin]);
+            }
+             col_Begin++;
         }
         
-        return res;
+        
+        return list;
     }
 }
