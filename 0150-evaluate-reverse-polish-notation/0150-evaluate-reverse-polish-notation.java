3ad3316b3d8b2e53591dab1427deaa3c9ad48e53
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2023-12-16 17:25:55 +0530

    Time: 6 ms (77.33%), Space: 43.4 MB (42.97%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -10,13 +10,13 @@ class Solution {
                 stack.push(a+b);
             }
             
-           else if(token.equals("-")){
+            else if(token.equals("-")){
                 int a = stack.pop();
                 int b = stack.pop();
                 stack.push(b-a);
             }
             
-             else if(token.equals("*")){
+            else if(token.equals("*")){
                 int a = stack.pop();
                 int b = stack.pop();
                 stack.push(a*b);
