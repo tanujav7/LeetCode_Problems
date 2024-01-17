10479ea965c07d4e01e7d25e6c0d49662bfc1178
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2024-01-17 11:04:51 +0530

    Time: 4 ms (67.52%), Space: 42.7 MB (76.72%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,22 +1,23 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         
-        int count[] = new int[26];
+        int countArr[] = new int[26];
         
         if(s.length()!=t.length())
             return false;
         
         for(int i=0; i<s.length(); i++){
-            count[s.charAt(i)-'a']++;
+            countArr[s.charAt(i)-'a']++;
         }
         
+        
         for(int i=0; i<t.length(); i++){
-            if(count[t.charAt(i)-'a']==0)
+            if(countArr[t.charAt(i)-'a']==0)
                 return false;
             else
-                count[t.charAt(i)-'a']--;
+                countArr[t.charAt(i)-'a']--;
         }
-            
+        
         return true;
     }
 }
