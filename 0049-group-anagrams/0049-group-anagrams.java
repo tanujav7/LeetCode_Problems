7f0f2e561ff5603a1b0b2187452a0e80118c476b
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2024-02-06 19:19:56 +0530

    Time: 7 ms (57.75%), Space: 47.5 MB (65.51%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,6 +1,5 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        
         Map<String, List<String>> map = new HashMap<>();
         
         for(int i=0; i<strs.length; i++){
