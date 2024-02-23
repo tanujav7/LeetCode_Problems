1fc3b4b2afce7a0b9ac6f8111c113823f6f996c9
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2024-02-23 20:01:59 +0530

    Time: 0 ms (100.00%), Space: 42 MB (26.77%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,36 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        
+        int n = nums.length;
+        
+        int count0 = 0, count1 = 0, count2 = 0;
+        
+        for(int i=0; i<n; i++){
+            switch(nums[i]){
+                case 0 : count0++;
+                    break;
+                case 1 : count1++;
+                    break;
+                case 2 : count2++;
+                    break;
+            }
+        }
+        
+        
+        int k = 0;
+        
+        while(count0-->0)
+            nums[k++] = 0;
+        
+         
+        while(count1-->0)
+            nums[k++] = 1;
+        
+         
+        while(count2-->0)
+            nums[k++] = 2;
+        
+        
+        //return nums
+    }
+}
