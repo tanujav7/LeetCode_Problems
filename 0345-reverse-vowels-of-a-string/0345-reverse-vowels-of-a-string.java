ecb40ea478a03f4207ca463ecf060a72dd838fdc
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2024-02-24 17:10:08 +0530

    Time: 4 ms (59.32%), Space: 44.6 MB (90.10%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -2,6 +2,7 @@ class Solution {
     public String reverseVowels(String s) {
         
         Set<Character> set = new HashSet<>();
+        
         set.add('a');
         set.add('e');
         set.add('i');
@@ -14,26 +15,27 @@ class Solution {
         set.add('U');
         
         char arr[] = s.toCharArray();
-        int i = 0, j = arr.length-1;
+        
+        int i=0, j=s.length()-1;
         
         while(i<j){
+            if(!set.contains(arr[i]))
+                i++;
             
-         while(i<j && !set.contains(arr[i]))
-             i++;
+            else if(!set.contains(arr[j]))
+                j--;
             
-         while(i<j &&!set.contains(arr[j]))
-             j--;
-            
-             char temp = arr[i];
-             arr[i] = arr[j];
-             arr[j] = temp;
-         
-             i++;
-             j--;
-          
-       }
+            else{
+                char temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }
         
-       String str = new String(arr);
-       return str;
+        String res = new String(arr);
+        
+        return res;
     }
 }
