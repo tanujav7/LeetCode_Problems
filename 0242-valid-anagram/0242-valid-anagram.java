846e968281245ca1229eefca59a5a645d617a179
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2024-02-29 11:09:10 +0530

    Time: 3 ms (88.98%), Space: 42.6 MB (94.71%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,26 +1,25 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         
-        if(s.length()!=t.length()){
+        int freArr[] = new int[26];
+        
+        if(s.length()!=t.length())
             return false;
-        }
-        
-        
-        int charFreq[] = new int[26];
         
         for(int i=0; i<s.length(); i++){
-            charFreq[s.charAt(i)-'a']++;
+            freArr[s.charAt(i)-'a']++;
         }
         
-        for(int i=0; i<t.length(); i++){
-            charFreq[t.charAt(i)-'a']--;
+        for(int i=0; i<s.length(); i++){
+            freArr[t.charAt(i)-'a']--;
         }
         
         for(int i=0; i<26; i++){
-            if(charFreq[i]!=0)
+            if(freArr[i]!=0)
                 return false;
         }
-    
+        
+        
         return true;
     }
 }
