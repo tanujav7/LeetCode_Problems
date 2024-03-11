02e1ff377a7349fc968eb4e0d887f83ed5983eb5
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2024-03-11 16:48:37 +0530

    Time: 6 ms (11.51%), Space: 42.1 MB (10.73%) - LeetHub

---
+++ 0791-custom-sort-string/0791-custom-sort-string.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String customSortString(String order, String s) {
+         int N = s.length();
+        Character[] result = new Character[N];
+        for (int i = 0; i < N; i++) {
+            result[i] = s.charAt(i);
+        }
+
+        // Define the custom comparator
+        Arrays.sort(result, (c1, c2) -> {
+            // The index of the character in order determines the value to be sorted by
+            return order.indexOf(c1) - order.indexOf(c2);
+        });
+
+        // Return the result
+        String resultString = "";
+        for (Character c: result) {
+            resultString += c;
+        }
+        return resultString;
+    }
+}
