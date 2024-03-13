24fb308155bb50d7948189b3390fa7942215b58d
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2024-03-13 21:05:28 +0530

    Time: 2 ms (54.25%), Space: 41.8 MB (12.45%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -6,16 +6,19 @@ class Solution {
         for(int i=0; i<s.length(); i++){
             char ch = s.charAt(i);
             
-            if(ch=='[' || ch=='{' || ch=='(')
+            if(ch=='(' || ch=='{' || ch=='[')
                 stack.push(ch);
             
             else if(!stack.isEmpty()){
-                if(ch=='}' && stack.peek()=='{')
+                if(stack.peek()=='(' && ch==')')
                     stack.pop();
-                else if(ch==']' && stack.peek()=='[')
+                
+                else if(stack.peek()=='[' && ch==']')
                     stack.pop();
-               else if(ch==')' && stack.peek()=='(')
+                
+                 else if(stack.peek()=='{' && ch=='}')
                     stack.pop();
+                
                 else
                     stack.push(ch);
             }
