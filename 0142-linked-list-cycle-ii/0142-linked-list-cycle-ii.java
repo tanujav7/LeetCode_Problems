ff78b76688041d639fe1b1652438445a431aae2e
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2024-03-15 21:14:42 +0530

    Time: 0 ms (100.00%), Space: 44.7 MB (17.60%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -15,7 +15,6 @@ public class Solution {
         ListNode slow = head, fast = head;
         
         while(fast!=null && fast.next!=null){
-            
             slow = slow.next;
             fast = fast.next.next;
             
