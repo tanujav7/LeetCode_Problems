7a837e1a2c1b7bbca83e2329db167cc573bc4592
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2024-03-31 00:43:31 +0530

    Time: 2 ms (68.34%), Space: 44.5 MB (57.54%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,31 +1,39 @@
 class Solution {
+    
     public int maxAreaOfIsland(int[][] grid) {
         
         int row = grid.length;
         int col = grid[0].length;
         
-        int maxIsland = 0;
+        int maxArea = 0;
+        int count = 0;
         
         for(int i=0; i<row; i++){
+            //int k = 0;
+            count = 0;
             for(int j=0; j<col; j++){
                 if(grid[i][j]==1){
-                    maxIsland = Math.max(maxIsland, dfs(grid, i, j));
+                   maxArea = Math.max(dfs(grid, i, j), maxArea);
+                   // count = dfs(grid, i, j);
                 }
             }
+           
         }
         
-        return maxIsland;
+        return maxArea;
     }
     
     
     int dfs(int grid[][], int i, int j){
-        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length || grid[i][j]==0 || grid[i][j]==2)
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]==2 || grid[i][j]==0){
             return 0;
+        }
         
         
-        grid[i][j] = 2;
+         grid[i][j] = 2;
+      
         
-        return (1 + dfs(grid, i-1, j) + dfs(grid, i+1, j) + dfs(grid, i, j-1) + dfs(grid, i, j+1));
-            
+        return 1 + dfs(grid, i-1, j) + dfs(grid, i+1, j) + dfs(grid, i, j-1) + dfs(grid, i, j+1);
+        
     }
 }
