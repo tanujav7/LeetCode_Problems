7bfb5a901b74776f3b31fbd78eca4853be1eb73d
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2024-03-30 23:42:12 +0530

    Time: 2 ms (79.19%), Space: 45.9 MB (87.47%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
@@ -1,27 +1,33 @@
 class Solution {
     List<List<Integer>> resList;
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        
         resList = new ArrayList<>();
-        List<Integer> path = new ArrayList<>();
+        int n = graph.length;
         int src = 0;
-        int dest = graph.length-1;
-        path.add(0);
-        dfs(graph, src, dest, path);
+        int dest = n-1;
+        
+        boolean visited[] = new boolean[n];
+        
+        getPaths(graph, src, dest, visited, new ArrayList<>());
         return resList;
     }
     
-    void dfs(int graph[][], int src, int dest, List<Integer>path){
+    void getPaths(int graph[][], int src, int dest, boolean visited[], List<Integer> path){
+        visited[src] = true;
+        path.add(src);
+        
         if(src==dest){
             resList.add(new ArrayList<>(path));
-            return;
         }
         
-        else{
-            for(int ele : graph[src]){
-                path.add(ele);
-                dfs(graph, ele, dest, path);
-                path.remove(path.size()-1);
+        for(int ele : graph[src]){
+            if(!visited[ele]){
+                getPaths(graph, ele, dest, visited, path);
             }
         }
+        
+        visited[src] = false;
+        path.remove(path.size()-1);
     }
 }
