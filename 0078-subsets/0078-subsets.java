e96b7c9f443776da24ecb59db7e7f0fabefe7852
Tanuja V <73555975+tanujav988@users.noreply.github.com>
2024-04-07 00:18:06 +0530

    Time: 1 ms (61.10%), Space: 42.8 MB (19.65%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -2,21 +2,21 @@ class Solution {
     List<List<Integer>> resList;
     public List<List<Integer>> subsets(int[] nums) {
         resList = new ArrayList<>();
-        backTrack(nums, 0, new ArrayList<>());
+        backTrack(nums, 0, nums.length, new ArrayList<>());
         return resList;
     }
     
-    void backTrack(int nums[], int i, List<Integer> list){
-        if(i==nums.length){
-            resList.add(new ArrayList<>(list));
+    void backTrack(int nums[], int i, int n, List<Integer> path){
+        if(i==n){
+            resList.add(new ArrayList<>(path));
             return;
         }
         
         else{
-            list.add(nums[i]);
-            backTrack(nums, i+1, list);
-            list.remove(list.size()-1);
-            backTrack(nums, i+1, list);
+            path.add(nums[i]);
+            backTrack(nums, i+1, n, path);
+            path.remove(path.size()-1);
+            backTrack(nums, i+1, n, path);
         }
     }
 }
