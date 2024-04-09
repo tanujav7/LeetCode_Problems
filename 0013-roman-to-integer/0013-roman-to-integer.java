d32792d5e9da06e51522346ae0941f5150eb65a5
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-04-09 18:39:56 +0530

    Time: 3 ms (83.10%), Space: 44.5 MB (78.40%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -2,36 +2,40 @@ class Solution {
     public int romanToInt(String s) {
         
         int n = s.length();
+        
+        int i = 0;
+        
         int nums[] = new int[n];
         
-        for(int i=0; i<n; i++){
-            switch(s.charAt(i)){
-                case 'I' : nums[i] = 1;
+        for(char ch : s.toCharArray()){
+            switch(ch){
+                case 'I' : nums[i++] = 1;
                     break;
-                case 'V' : nums[i] = 5;
+                case 'V' : nums[i++] = 5;
                     break;
-                case 'X' : nums[i] = 10;
+                case 'X' : nums[i++] = 10;
                     break;
-                case 'L' : nums[i] = 50;
+                case 'L' : nums[i++] = 50;
                     break;
-                case 'C' : nums[i] = 100;
+                case 'C' : nums[i++] = 100;
                     break;
-                case 'D' : nums[i] = 500;
+                case 'D' : nums[i++] = 500;
                     break;
-                case 'M' : nums[i] = 1000;
+                case 'M' : nums[i++] = 1000;
                     break;
             }
         }
         
         int sum = 0;
         
-        for(int i=0; i<n-1; i++){
-            if(nums[i+1]>nums[i])
-                sum = sum - nums[i];
+        for(int j=0; j<n-1; j++){
+            if(nums[j+1]>nums[j])
+                sum -= nums[j];
             else
-                sum = sum + nums[i];
+                sum += nums[j];
         }
         
-        return sum + nums[n-1];
+        return sum+nums[n-1];
+        
     }
 }
