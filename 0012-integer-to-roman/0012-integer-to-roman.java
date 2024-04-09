b056eeca11123ad5feeb619960c6e64120616284
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-04-09 14:00:41 +0530

    Time: 4 ms (56.89%), Space: 44.4 MB (48.18%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,9 +1,12 @@
 class Solution {
     public String intToRoman(int num) {
         
+        String romanLiteral[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        
+        int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4 ,1};
+        
+        
         String res = "";
-        String romanLiteral[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
-        int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         
         for(int i=0; i<values.length; i++){
             while(num>=values[i]){
@@ -11,7 +14,7 @@ class Solution {
                 res = res + romanLiteral[i];
             }
         }
-            
+        
         return res;
     }
 }
