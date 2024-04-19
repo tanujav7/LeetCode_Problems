4258e028f55b34eb2504b1027390b90304900c9c
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-04-19 14:50:27 +0530

    Time: 0 ms (100.00%), Space: 45.4 MB (83.17%) - LeetHub

--- 0704-binary-search/0704-binary-search.java
+++ 0704-binary-search/0704-binary-search.java
@@ -12,12 +12,14 @@ class Solution {
         int mid = low+(high-low)/2;
         
         if(nums[mid]<target)
-            return binarySearch(nums, mid+1, high, target);
+           return binarySearch(nums, mid+1, high, target);
         
        else if(nums[mid]>target)
-            return binarySearch(nums, low, mid-1, target);
+           return binarySearch(nums, low, mid-1, target);
         
         else
             return mid;
+        
+     
     }
 }
