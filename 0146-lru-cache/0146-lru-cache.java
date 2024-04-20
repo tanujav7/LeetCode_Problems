32ce8299caba7087f82f1b095a1b9aed44d6e60c
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-04-20 13:22:30 +0530

    Time: 55 ms (19.90%), Space: 114.2 MB (47.14%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -3,18 +3,17 @@ class LRUCache {
     Node head = new Node(0, 0);
     Node tail = new Node(0, 0);
     
+    int capacity = 0;
     Map<Integer, Node> map = new HashMap<>();
-    int capacity;
     
     public LRUCache(int capacity) {
-        
         this.capacity = capacity;
         head.next = tail;
         tail.prev = head;
     }
     
     public int get(int key) {
-         if(map.containsKey(key)){
+        if(map.containsKey(key)){
             Node node = map.get(key);
             remove(node);
             insert(node);
@@ -26,7 +25,6 @@ class LRUCache {
     }
     
     public void put(int key, int value) {
-        
         if(map.containsKey(key))
             remove(map.get(key));
         
@@ -34,15 +32,6 @@ class LRUCache {
             remove(tail.prev);
         
         insert(new Node(key, value));
-       
-    }
-    
-    
-    
-    void remove(Node node){
-        map.remove(node.key);
-        node.prev.next = node.next;
-        node.next.prev = node.prev;
     }
     
     
@@ -53,7 +42,12 @@ class LRUCache {
         head.next = node;
         node.prev = head;
     }
-
+    
+    void remove(Node node){
+        map.remove(node.key);
+        node.next.prev = node.prev;
+        node.prev.next = node.next;
+    }
 }
 
 class Node{
