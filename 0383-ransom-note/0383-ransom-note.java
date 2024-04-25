7a92fb6a34b04da5a732d2268989824fd0e3ede6
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-04-25 19:25:15 +0530

    Time: 11 ms (47.41%), Space: 44.5 MB (73.32%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -7,11 +7,12 @@ class Solution {
             map.put(ch, map.getOrDefault(ch, 0)+1);
         }
         
-        
-        for(char ch : ransomNote.toCharArray()){
+         for(char ch : ransomNote.toCharArray()){
             int count = map.containsKey(ch) ? map.get(ch)-1 : -1;
+             
             if(count==-1)
                 return false;
+             
             map.put(ch, count);
         }
         
