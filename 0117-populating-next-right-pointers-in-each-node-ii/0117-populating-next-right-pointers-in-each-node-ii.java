05320c376db0db9449b34ee3e64323d95bfa859a
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-04-25 13:32:16 +0530

    Time: 2 ms (43.63%), Space: 44.4 MB (15.76%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.java
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.java
@@ -24,29 +24,27 @@ class Node {
 class Solution {
     public Node connect(Node root) {
         
-        Queue<Node> queue = new LinkedList<>();
-        
-        queue.add(root);
-        
         if(root==null)
             return null;
         
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        
         while(!queue.isEmpty()){
-            
             int size = queue.size();
             
             for(int i=0; i<size; i++){
                 Node node = queue.remove();
                 
-                if(i<size-1)
+                if(i<size-1){
                     node.next = queue.peek();
+                }
                 
                 if(node.left!=null)
                     queue.add(node.left);
                 
                 if(node.right!=null)
                     queue.add(node.right);
-                
             }
         }
         
