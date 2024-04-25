1d853e1678d4d9c4463dd4f5e1e5fc5cf04745fb
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-04-25 19:20:14 +0530

    Time: 10 ms (57.96%), Space: 45.2 MB (15.66%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -3,20 +3,16 @@ class Solution {
         
         Map<Character, Integer> map = new HashMap<>();
         
-        for(int i=0; i<magazine.length(); i++){
-            char ch = magazine.charAt(i);
-            if(map.containsKey(ch))
-                map.put(ch,map.get(ch)+1);
-            else
-                map.put(ch,1);
+        for(char ch : magazine.toCharArray()){
+            map.put(ch, map.getOrDefault(ch, 0)+1);
         }
         
-        for(int i=0; i<ransomNote.length(); i++){
-            char ch = ransomNote.charAt(i);
-            int count = (map.containsKey(ch)) ? map.get(ch)-1 : -1;
+        
+        for(char ch : ransomNote.toCharArray()){
+            int count = map.containsKey(ch) ? map.get(ch)-1 : -1;
             if(count==-1)
                 return false;
-            map.put(ch,count);
+            map.put(ch, count);
         }
         
         return true;
