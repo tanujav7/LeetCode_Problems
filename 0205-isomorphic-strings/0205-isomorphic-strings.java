4832e361d3ea2d6c1e8e3c69333795d76fe22dfa
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-04-25 18:18:23 +0530

    Time: 27 ms (6.11%), Space: 41.9 MB (96.41%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -4,9 +4,8 @@ class Solution {
         Map<Character, Character> map_S = new HashMap<>();
         Map<Character, Character> map_T = new HashMap<>();
         
-        if(s.length()!=t.length()){
+        if(s.length()!=t.length())
             return false;
-        }
         
         for(int i=0; i<s.length(); i++){
             char s_ch = s.charAt(i);
@@ -15,7 +14,7 @@ class Solution {
             if(map_S.containsKey(s_ch) && !(map_S.get(s_ch).equals(t_ch)))
                 return false;
             
-            if(map_T.containsKey(t_ch) && !(map_T.get(t_ch).equals(s_ch)))
+             if(map_T.containsKey(t_ch) && !(map_T.get(t_ch).equals(s_ch)))
                 return false;
             
             map_S.put(s_ch, t_ch);
