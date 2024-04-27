7e821352dc87e75ab09a90807cec8da0d25d7572
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-04-28 01:11:19 +0530

    Time: 1 ms (100.00%), Space: 44.3 MB (50.08%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -11,32 +11,29 @@
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         
+        int carry = 0;
+        
+
         ListNode dummyHead = new ListNode(0);
         
-        ListNode current = dummyHead;
-        
-        int sum = 0, carry = 0;
+        ListNode currentNode = dummyHead;
         
         while(l1!=null || l2!=null || carry>0){
+            int sum1 = (l1!=null) ? l1.val : 0;
+            int sum2 = (l2!=null) ? l2.val : 0;
             
-            int s1 = (l1!=null) ? l1.val : 0;
+            int sum = sum1 + sum2 + carry;
             
-            int s2 = (l2!=null) ? l2.val : 0;
-            
-            sum = s1 + s2 + carry;
-            
-            current.next = new ListNode(sum%10);
+            currentNode.next = new ListNode(sum%10);
+            currentNode = currentNode.next;
             
             carry = sum/10;
-            
-            current = current.next;
             
             if(l1!=null)
                 l1 = l1.next;
             
             if(l2!=null)
                 l2 = l2.next;
-            
         }
         
         return dummyHead.next;
