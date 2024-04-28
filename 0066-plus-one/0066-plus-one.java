70634cdd93b2e853c102f8adcfbf81f53feee411
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-04-28 10:40:53 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (58.86%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -2,22 +2,24 @@ class Solution {
     public int[] plusOne(int[] digits) {
         
         int n = digits.length;
+        
         int newDigits[] = new int[n+1];
         
         int carry = 0;
+        
         for(int i=n-1; i>=0; --i){
             int currentSum = digits[i] + 1;
             
             if(currentSum>9){
                 carry = 1;
-                newDigits[i+1] = currentSum%10;
-                digits[i] = currentSum%10;
+                digits[i] = currentSum % 10;
+                newDigits[i+1] = currentSum % 10;
             }
             
             else{
                 carry = 0;
-                newDigits[i+1] = currentSum;
                 digits[i] = currentSum;
+                newDigits[i+1] = currentSum;
                 break;
             }
         }
