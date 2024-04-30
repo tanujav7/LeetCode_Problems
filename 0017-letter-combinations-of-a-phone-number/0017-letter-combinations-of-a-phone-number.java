6f27ea71ba4fee9f61e9ac184975fe05d00df4cd
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-04-30 12:34:06 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (38.15%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -2,28 +2,27 @@ class Solution {
     List<String> resList;
     public List<String> letterCombinations(String digits) {
         resList = new ArrayList<>();
-        int n = digits.length();
         
-        if(n==0)
+        if(digits.isEmpty())
             return resList;
         
-        backTrack(new StringBuilder(), n, 0, digits);
-        
+        backTrack(new StringBuilder(), digits.length(), 0, digits);
         return resList;
     }
     
     String digitsToLetters[] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
     
-    void backTrack(StringBuilder sb, int n, int index, String digits){
+    void backTrack(StringBuilder sb, int n, int ind, String digits){
         if(sb.length()==n){
             resList.add(sb.toString());
             return;
         }
         
-        for(char ch : digitsToLetters[digits.charAt(index)-'0'].toCharArray()){
+        for(char ch : digitsToLetters[digits.charAt(ind)-'0'].toCharArray()){
             sb.append(ch);
-            backTrack(sb, n, index+1, digits);
+            backTrack(sb, n, ind+1, digits);
             sb.deleteCharAt(sb.length()-1);
         }
     }
+    
 }
