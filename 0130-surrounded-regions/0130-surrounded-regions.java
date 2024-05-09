9fd8e86ff7733891f5947d116923bfed29900de3
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-05-09 16:56:52 +0530

    Time: 1 ms (98.94%), Space: 45.1 MB (71.78%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -2,50 +2,51 @@ class Solution {
     public void solve(char[][] board) {
         
         int row = board.length;
+        
         int col = board[0].length;
         
-         //For first row & last row
+        
+        //First row and last row      
         for(int j=0; j<col; j++){
             if(board[0][j]=='O')
-            dfs(board, 0, j);
+                dfs(board, 0, j);
             
             if(board[row-1][j]=='O')
-            dfs(board, row-1, j);
+                 dfs(board, row-1, j);
         }
         
-        //For first column & last column
+        
+         //First column and last column      
         for(int j=0; j<row; j++){
             if(board[j][0]=='O')
-            dfs(board, j, 0);
+                dfs(board, j, 0);
             
             if(board[j][col-1]=='O')
-            dfs(board, j, col-1);
+                 dfs(board, j, col-1);
         }
+        
+        
         
         for(int i=0; i<row; i++){
             for(int j=0; j<col; j++){
-                if(board[i][j]=='#'){
+                if(board[i][j]=='#')
                     board[i][j] = 'O';
-                }
-               else if(board[i][j]=='O'){
+                else
                     board[i][j] = 'X';
-                }
             }
         }
         
-        
     }
     
-    void dfs(char grid[][], int i, int j){
-        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length ||  grid[i][j]=='X' || grid[i][j]=='#')
+    void dfs(char board[][], int i, int j){
+        if(i<0 || j<0 || i>=board.length || j>=board[0].length || board[i][j]=='X' || board[i][j]=='#')
             return;
         
-        grid[i][j] = '#';
+        board[i][j] = '#';
         
-        dfs(grid, i, j+1);
-        dfs(grid, i, j-1);
-        dfs(grid, i+1, j);
-        dfs(grid, i-1, j);
+        dfs(board, i+1, j);
+        dfs(board, i-1, j);
+        dfs(board, i, j+1);
+        dfs(board, i, j-1);
     }
 }
-
