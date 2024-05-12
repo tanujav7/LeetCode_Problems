b203e73859d13d84684ff08c044c7304adf53582
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-05-12 20:06:01 +0530

    Time: 1 ms (100.00%), Space: 42.4 MB (36.40%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,32 +1,27 @@
 class Solution {
     public String addBinary(String a, String b) {
-        
         int i = a.length()-1;
-        
         int j = b.length()-1;
-        
-        StringBuilder sb = new StringBuilder();
-        
         int carry = 0;
-        
         int n1 = 0, n2 = 0;
+        StringBuilder sb = new StringBuilder();
         
         while(i>=0 || j>=0 || carry>0){
             if(i>=0){
-                 n1 = a.charAt(i--)-'0';
+                n1 = (a.charAt(i--)-'0');
             }
             else{
                 n1 = 0;
             }
             
             if(j>=0){
-                 n2 = b.charAt(j--)-'0';
+                n2 = (b.charAt(j--)-'0');
             }
             else{
                 n2 = 0;
             }
             
-            int sum = n1 + n2 + carry;
+            int sum = carry + n1 + n2;
             
             sb.append(sum % 2);
             
@@ -34,6 +29,5 @@ class Solution {
         }
         
         return sb.reverse().toString();
-        
     }
 }
