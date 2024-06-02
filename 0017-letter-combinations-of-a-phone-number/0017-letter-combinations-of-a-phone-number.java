6f2cec0aaa460e764ff858b6a897005bc51f7054
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-06-02 11:49:13 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (84.56%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,32 +1,28 @@
 class Solution {
-    List<String> res;
+    List<String> list = new ArrayList<>();
     public List<String> letterCombinations(String digits) {
-        res = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
         int n = digits.length();
         
         if(n==0)
-            return res;
-        
-        StringBuilder sb = new StringBuilder();
+            return list;
         
         backTrack(digits, sb, 0, n);
-        
-        return res;
+        return list;
     }
     
-    String digitsToLetters[] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    String digitsToLetters[] = {"", "", "abc", "def", "ghi", "jkl" ,"mno", "pqrs", "tuv", "wxyz"};
     
-    void backTrack(String digits, StringBuilder sb, int i, int n){
+    void backTrack(String digits, StringBuilder sb, int index, int n){
         if(sb.length()==n){
-            res.add(sb.toString());
+            list.add(sb.toString());
             return;
         }
         
-        for(char ch : digitsToLetters[digits.charAt(i)-'0'].toCharArray()){
+        for(char ch : digitsToLetters[digits.charAt(index)-'0'].toCharArray()){
             sb.append(ch);
-            backTrack(digits, sb, i+1, n);
+            backTrack(digits, sb, index+1, n);
             sb.deleteCharAt(sb.length()-1);
         }
-        
     }
 }
