79fe1b1b6bce226590d8c8f2ae344189fd422f4f
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-06-05 13:30:31 +0530

    Time: 2 ms (100.00%), Space: 44.4 MB (43.57%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.java
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.java
@@ -1,25 +1,22 @@
 class Solution {
     public int closedIsland(int[][] grid) {
         
-        int count = 0;
-        
         int row = grid.length;
+        
         int col = grid[0].length;
         
-        int move_x[] = {0, 1, 0, -1};
-        int move_y[] = {-1, 0, 1, 0};
+        int count = 0;
+        
         
         for(int i=0; i<row; i++){
             for(int j=0; j<col; j++){
-                if(grid[i][j]==0 && dfs(grid, i, j)){
+                if(grid[i][j]==0 && dfs(grid, i, j))
                     count++;
-                }
             }
         }
         
         return count;
     }
-    
     
     boolean dfs(int grid[][], int i, int j){
         if(i<0 || j<0 || i>=grid.length || j>=grid[0].length)
@@ -29,13 +26,12 @@ class Solution {
             return true;
         
         grid[i][j] = 2;
-       
-        boolean up = dfs(grid, i - 1, j);
-        boolean down = dfs(grid, i + 1, j);
-        boolean left = dfs(grid, i, j - 1);
-        boolean right = dfs(grid, i, j + 1);
-
-        // The current cell is part of a closed island only if all directions are closed
-        return up && down && left && right;
+        
+        boolean top = dfs(grid, i+1, j);
+        boolean down = dfs(grid, i-1, j);
+        boolean left = dfs(grid, i, j-1);
+        boolean right = dfs(grid, i, j+1);
+        
+        return (top && down && left && right);
     }
 }
