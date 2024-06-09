4d6aec0383fbf6667a5b7e86896b40f275f9084c
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-06-09 11:12:38 +0530

    Time: 2 ms (100.00%), Space: 43.9 MB (91.55%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.java
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.java
@@ -2,16 +2,15 @@ class Solution {
     public int closedIsland(int[][] grid) {
         
         int row = grid.length;
-        
         int col = grid[0].length;
         
         int count = 0;
         
-        
         for(int i=0; i<row; i++){
             for(int j=0; j<col; j++){
-                if(grid[i][j]==0 && dfs(grid, i, j))
+                if(grid[i][j]==0 && dfs(grid, i, j)){
                     count++;
+                }
             }
         }
         
