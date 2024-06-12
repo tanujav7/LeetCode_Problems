096a32279d3b487c3b9315fce4f6c9a0eb42f2c4
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-06-12 10:01:41 +0530

    Time: 3 ms (60.78%), Space: 53.5 MB (66.15%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.java
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -1,21 +1,22 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
         
-        int adjList[] = new int[n+1];
+        int indegree[] = new int[n+1];
         
         for(int node[] : trust){
             int n1 = node[0];
             int n2 = node[1];
             
-            adjList[n1]--;
-            adjList[n2]++;
+            indegree[n1]--;
+            indegree[n2]++;
         }
         
         
         for(int i=1; i<=n; i++){
-            if(adjList[i]==n-1)
+            if(indegree[i]==n-1)
                 return i;
         }
+        
         
         return -1;
     }
