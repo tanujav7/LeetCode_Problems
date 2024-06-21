535e294c94d7ee8d884b268a815bc108ebaf3cba
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-06-21 16:56:39 +0530

    Time: 3 ms (93.92%), Space: 44 MB (91.77%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,40 +1,45 @@
 class Solution {
-
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < numCourses; i++) {
-            adj.add(new ArrayList<>());
+        
+        List<List<Integer>> adjList = new ArrayList<>();
+        
+        for(int i=0; i<numCourses; i++){
+            adjList.add(new ArrayList<>());
         }
-
-        for (int i = 0; i < prerequisites.length; i++) {
-            adj.get(prerequisites[i][0]).add(prerequisites[i][1]);
+        
+        for(int i=0; i<prerequisites.length; i++){
+            adjList.get(prerequisites[i][0]).add(prerequisites[i][1]);
         }
-
-        int[] visited = new int[numCourses];
-        for (int i = 0; i < numCourses; i++) {
-            if (visited[i] == 0) {
-                if (isCyclic(adj, visited, i)) {
+        
+        
+        int visited[] = new int[numCourses];
+        
+        for(int i=0; i<numCourses; i++){
+            if(visited[i]==0){
+                if(isCyclic(adjList, i, numCourses, visited))
                     return false;
-                }
             }
         }
+        
         return true;
     }
-
-    private boolean isCyclic(List<List<Integer>> adj, int[] visited, int curr) {
-        if (visited[curr] == 2) {
-            return true;
+    
+    boolean isCyclic(List<List<Integer>> adjList, int u, int numCourses, int visited[]){
+        
+     if(visited[u]==2)
+         return true;
+        
+        visited[u] = 2;
+        
+        for(int i=0; i<adjList.get(u).size(); i++){
+            int v = adjList.get(u).get(i);
+            if(visited[v]!=1 && isCyclic(adjList, v, numCourses, visited))
+                return true;  
         }
-
-        visited[curr] = 2;
-        for (int i = 0; i < adj.get(curr).size(); i++) {
-            if (visited[adj.get(curr).get(i)] != 1) {
-                if (isCyclic(adj, visited, adj.get(curr).get(i))) {
-                    return true;
-                }
-            }
-        }
-        visited[curr] = 1;
+        
+        visited[u] = 1;
+        
         return false;
+        
     }
 }
