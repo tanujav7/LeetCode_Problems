5cddbc0780347ddce96bcba15b3958cbf428ec3e
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-10-06 01:12:51 +0530

    Time: 0 ms (100.00%), Space: 47.6 MB (38.17%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -15,10 +15,9 @@
  */
 class Solution {
     public int countNodes(TreeNode root) {
-        
         if(root==null)
             return 0;
         
-        return (1 + countNodes(root.left) + countNodes(root.right));
+        return 1 + countNodes(root.left) + countNodes(root.right);
     }
 }
