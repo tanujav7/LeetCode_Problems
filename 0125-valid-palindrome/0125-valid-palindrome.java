03548846a6740855e5fa91bcf688f6ee47f6e05a
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-10-26 00:15:38 +0530

    Time: 3 ms (73.06%), Space: 43.1 MB (63.82%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -7,9 +7,9 @@ class Solution {
         int i = 0, j = s.length()-1;
         
         while(i<j){
-            while(i<j && !((s.charAt(i)>='a' && s.charAt(i)<='z') || (s.charAt(i)>='0' && s.charAt(i)<='9')))
+             while(i<j && !((s.charAt(i)>='a' && s.charAt(i)<='z') || (s.charAt(i)>='0' && s.charAt(i)<='9')))
                 i++;
-            while(i<j && !((s.charAt(j)>='a' && s.charAt(j)<='z') || (s.charAt(j)>='0' && s.charAt(j)<='9')))
+             while(i<j && !((s.charAt(j)>='a' && s.charAt(j)<='z') || (s.charAt(j)>='0' && s.charAt(j)<='9')))
                 j--;
             if(s.charAt(i)!=s.charAt(j))
                 return false;
