faa7c31bd9cad1693f53de8ad9d19b0ebe176866
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-10-30 12:33:09 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (55.51%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -15,22 +15,19 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        
         return checkSymmetric(root.left, root.right);
-        
     }
     
     boolean checkSymmetric(TreeNode p, TreeNode q){
         if(p==null && q==null)
             return true;
         
-         if(p==null || q==null)
+        if(p==null && q!=null || p!=null && q==null)
             return false;
         
         if(p.val!=q.val)
             return false;
         
-        
-        return (checkSymmetric(p.left, q.right) && checkSymmetric(q.left, p.right));
+        return (checkSymmetric(p.left, q.right) && checkSymmetric(p.right, q.left));
     }
 }
