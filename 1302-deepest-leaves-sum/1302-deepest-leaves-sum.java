ad3bbb8473224e223560cad483883a9dd0e23256
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-11-04 18:29:01 +0530

    Time: 7 ms (59.09%), Space: 46.2 MB (53.81%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
@@ -14,30 +14,25 @@
  * }
  */
 class Solution {
-    int sum = 0;
     public int deepestLeavesSum(TreeNode root) {
-        int maxDepth = maxDepth(root);
-        calculateSum(root, maxDepth, 1);
-        return sum;
-    }
-    
-    int maxDepth(TreeNode node){
-        if(node==null)
-            return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int sum = 0;
         
-        return 1 + Math.max(maxDepth(node.left), maxDepth(node.right));
-    }
-    
-    void calculateSum(TreeNode node, int maxDepth, int depth){
-        if(node==null)
-            return;
-        
-        if(node.left==null && node.right==null && maxDepth==depth){
-            sum += node.val;
-            //return sum;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            sum = 0;
+            for(int i=0; i<size; i++){
+                TreeNode currentNode = queue.remove();
+                 sum = sum + currentNode.val;
+                if(currentNode.left!=null)
+                    queue.add(currentNode.left);
+                
+                if(currentNode.right!=null)
+                    queue.add(currentNode.right);
+            }
         }
         
-        calculateSum(node.left, maxDepth, depth+1); 
-        calculateSum(node.right, maxDepth, depth+1);
+        return sum;
     }
 }
