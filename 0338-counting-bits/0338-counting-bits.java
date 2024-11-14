8a8fd5f2b745c6b2ac9e759668363e40271d7b80
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-11-15 01:20:46 +0530

    Time: 590 ms (5.65%), Space: 46.8 MB (13.20%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,29 +1,28 @@
 class Solution {
     public int[] countBits(int n) {
         
-        int arr[] = new int[n+1];
+        int res[] = new int[n+1];
         
         for(int i=0; i<=n; i++){
-            int oneCount = getOneCount(i);
-            arr[i] = oneCount;
+           res[i] = countOnes(i);
         }
         
-        return arr;
+        return res;
     }
     
-    int getOneCount(int n){
-        int res[] = new int[10005];
-        int i = 0;
+    int countOnes(int n){
+        int binary[] = new int[10000];
+        int i=0;
         
-        while(n>0){
-            res[i++] = n % 2;
+        while(n!=0){
+            binary[i++] = n % 2;
             n = n/2;
         }
         
         int count = 0;
         
-        for(int k : res){
-            if(k==1)
+        for(int j=0; j<binary.length; j++){
+            if(binary[j]==1)
                 count++;
         }
         
