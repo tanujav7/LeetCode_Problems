544fc836d67ff15ce0a7b840dd87ec030f8501f1
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-11-21 13:01:12 +0530

    Time: 25 ms (8.64%), Space: 42.7 MB (18.90%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,30 +1,28 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
+        
+        Map<Character, Character> map_S = new HashMap<>();
+        Map<Character, Character> map_T = new HashMap<>();
         int s_len = s.length();
+        
         int t_len = t.length();
         
         if(s_len!=t_len)
             return false;
         
-        Map<Character, Character> map_S = new HashMap<>();
-        
-        Map<Character, Character> map_T = new HashMap<>();
-        
         for(int i=0; i<s_len; i++){
-            char ch1 = s.charAt(i);
-            char ch2 = t.charAt(i);
+            char ch_S = s.charAt(i);
+            char ch_T = t.charAt(i);
             
-            if(map_S.containsKey(ch1) && !map_S.get(ch1).equals(ch2))
+            if(map_S.containsKey(ch_S) && !map_S.get(ch_S).equals(ch_T))
                 return false;
             
-            if(map_T.containsKey(ch2) && !map_T.get(ch2).equals(ch1))
+            if(map_T.containsKey(ch_T) && !map_T.get(ch_T).equals(ch_S))
                 return false;
             
-            map_S.put(ch1, ch2);
-            
-            map_T.put(ch2, ch1);
+            map_S.put(ch_S, ch_T);
+            map_T.put(ch_T, ch_S);
         }
-        
         
         return true;
     }
