86331f843a888a323f408bd647428c817c48e80e
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-12-01 13:33:46 +0530

    Time: 1 ms (76.48%), Space: 45.7 MB (34.67%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,16 +1,14 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-  
+        
         int R = matrix.length;
         int C = matrix[0].length;
-        
         
         boolean isCol = false;
         
         for(int i=0; i<R; i++){
             if(matrix[i][0]==0)
                 isCol = true;
-            
             for(int j=1; j<C; j++){
                 if(matrix[i][j]==0){
                     matrix[i][0] = 0;
@@ -19,25 +17,27 @@ class Solution {
             }
         }
         
+        
         for(int i=1; i<R; i++){
             for(int j=1; j<C; j++){
-                if(matrix[i][0]==0 || matrix[0][j]==0){
+                if(matrix[i][0]==0 || matrix[0][j]==0)
                     matrix[i][j] = 0;
-                }
             }
         }
         
-        
-        if(matrix[0][0]==0){
+         if(matrix[0][0]==0){
             for(int j=0; j<C; j++){
-                    matrix[0][j] = 0;
+                matrix[0][j] = 0;
             }
         }
         
-        if(isCol){
+        if(isCol==true){
             for(int i=0; i<R; i++){
-                    matrix[i][0] = 0;
+                matrix[i][0] = 0;
             }
         }
+        
+        
+       
     }
 }
