75f30faf42af62e2863d38528d86a142d3a69390
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-12-02 20:48:00 +0530

    Time: 1 ms (90.95%), Space: 48.2 MB (9.63%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -3,26 +3,26 @@ class Solution {
         
         int n = isConnected.length;
         
-        int visited[] = new int[n];
+        int visited[] = new int[200];
         
         int count = 0;
         
         for(int i=0; i<n; i++){
             if(visited[i]==0){
-                dfs(isConnected, visited, i, n);
                 count++;
+                dfs(isConnected, i, visited, n);
             }
         }
         
         return count;
     }
     
-    void dfs(int isConnected[][], int visited[], int u, int n){
+    void dfs(int isConnected[][], int u, int visited[], int n){
         visited[u] = 1;
         
-         for(int i=0; i<n; i++){
+        for(int i=0; i<n; i++){
             if(isConnected[u][i]==1 && visited[i]==0)
-             dfs(isConnected, visited, i, n);
-         }
+                dfs(isConnected, i, visited, n);
+        }
     }
 }
