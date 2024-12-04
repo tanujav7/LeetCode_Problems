82d17594d386fd7948543ecd343b5f9ebb53aab3
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-12-04 23:37:57 +0530

    Time: 2 ms (57.20%), Space: 44.4 MB (42.11%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.java
+++ 0841-keys-and-rooms/0841-keys-and-rooms.java
@@ -1,17 +1,20 @@
 class Solution {
     public boolean canVisitAllRooms(List<List<Integer>> rooms) {
         
-        Queue<Integer> queue = new LinkedList<>();
         int size = rooms.size();
+        
+        Queue<Integer> queue = new LinkedList<>();
+        
         int visited[] = new int[size];
         
         queue.add(0);
         visited[0] = 1;
         
+       
         
         while(!queue.isEmpty()){
             int node = queue.remove();
-            for(int adj : rooms.get(node)){
+            for(int adj:rooms.get(node)){
                 if(visited[adj]==0){
                     visited[adj] = 1;
                     queue.add(adj);
@@ -19,10 +22,10 @@ class Solution {
             }
         }
         
-        
         for(int visit : visited){
-            if(visit==0)
-               return false;
+            if(visit==0){
+                return false;
+            }
         }
         
         return true;
