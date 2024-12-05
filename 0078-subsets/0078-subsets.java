62e0a1a0fec828f08f2ca2319ac32f4fcb81e293
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-12-05 12:24:32 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (14.93%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,12 +1,9 @@
 class Solution {
     List<List<Integer>> resList = new ArrayList<>();
-    
     public List<List<Integer>> subsets(int[] nums) {
-        
         int n = nums.length;
         getSubsets(nums, 0, n, new ArrayList<>());
         return resList;
-        
     }
     
     void getSubsets(int nums[], int i, int n, List<Integer> list){
@@ -19,6 +16,5 @@ class Solution {
         getSubsets(nums, i+1, n, list);
         list.remove(list.size()-1);
         getSubsets(nums, i+1, n, list);
-        
     }
 }
