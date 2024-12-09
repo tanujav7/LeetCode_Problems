99673e6fd15b0625932325743a2b39e677b44c66
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-12-09 20:16:03 +0530

    Time: 1 ms (100.00%), Space: 42.7 MB (24.10%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -3,47 +3,37 @@ class Solution {
         
         s = s.trim();
         
-        if(s.equals(""))
-            return 0;
-        
         char sign = '+';
         
         int i = 0;
         
-        if(s.charAt(0)=='-'){
+        if(s.equals(""))
+            return 0;
+        
+        if(s.charAt(0)=='+'){
+            i++;
+        }
+        
+        else if(s.charAt(0)=='-'){
             sign = '-';
             i++;
         }
         
-     else if(s.charAt(0)=='+'){
-            sign = '+';
-            i++;
-        }
-        
-        else{
-            sign = '+';
-        }
-        
-        int n = s.length();
-        
         double res = 0;
         
-        while(i<n && s.charAt(i)>='0' && s.charAt(i)<='9'){
+        while(i<s.length() && s.charAt(i)>='0' && s.charAt(i)<='9'){
             res = res * 10 + s.charAt(i)-'0';
             i++;
         }
         
-        if(sign=='-'){
+        if(sign=='-')
             res = res * -1;
-        }
         
-        
-        if(res>Integer.MAX_VALUE)
+        if(res>=Integer.MAX_VALUE)
             return Integer.MAX_VALUE;
         
-        if(res<Integer.MIN_VALUE)
+         if(res<=Integer.MIN_VALUE)
             return Integer.MIN_VALUE;
-        
         
         return (int)res;
     }
