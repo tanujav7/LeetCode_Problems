b2808e38b78f0e0b871a179286a8ad20afda49cf
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-12-10 20:00:47 +0530

    Time: 6 ms (53.00%), Space: 42.8 MB (92.51%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -9,16 +9,16 @@ class Solution {
             arr[i] = String.valueOf(nums[i]);
         }
         
-        Arrays.sort(arr, (s1, s2)->(s2+s1).compareTo((s1+s2)));
+        Arrays.sort(arr, (s1, s2)->(s2+s1).compareTo(s1+s2));
         
         StringBuilder sb = new StringBuilder();
         
-        for(String s:arr){
-            sb.append(s);
-        }
-        
         if(arr[0].equals("0"))
             return "0";
+        
+        for(int i=0; i<n; i++){
+            sb.append(arr[i]);
+        }
         
         return sb.toString();
     }
