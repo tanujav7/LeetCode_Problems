401e01c6f70c1d44b8a3af1f7fd9c2fb785c9c3c
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-12-11 14:23:11 +0530

    Time: 1 ms (75.99%), Space: 45.9 MB (7.54%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -5,25 +5,38 @@ class Solution {
         
         int C = matrix[0].length;
         
-        Set<Integer> row = new HashSet<>();
-        
-        Set<Integer> col = new HashSet<>();
-        
+        boolean isCol = false;
         
         for(int i=0; i<R; i++){
-            for(int j=0; j<C; j++){
+            if(matrix[i][0]==0)
+                isCol = true;
+            for(int j=1; j<C; j++){
                 if(matrix[i][j]==0){
-                    row.add(i);
-                    col.add(j);
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
         
-        for(int i=0; i<R; i++){
-            for(int j=0; j<C; j++){
-                if(row.contains(i) || col.contains(j)){
-                    matrix[i][j] = 0;
+        
+        for(int i=1; i<R; i++){
+            for(int j=1; j<C; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j]=0;
                 }
+            }
+        }
+        
+        
+        if(matrix[0][0] == 0){
+            for(int j=0; j<C; j++){
+                matrix[0][j] = 0;
+            }
+        }
+        
+        if(isCol){
+            for(int i=0; i<R; i++){
+                matrix[i][0] = 0;
             }
         }
     }
