0a54f842d5806ea642471474cb5a7146054db9b8
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-12-12 21:28:45 +0530

    Time: 3 ms (66.49%), Space: 45.1 MB (49.25%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -15,25 +15,21 @@
  */
 class Solution {
     List<Integer> list = new ArrayList<>();
-    public boolean findTarget(TreeNode root, int x) {
-        inorderTraversal(root);
+    public boolean findTarget(TreeNode root, int k) {
         
+        inorderTraversal(root);
         int i=0, j=list.size()-1;
         
-       if(list.size()==1 && list.get(0)!=x){
-            return false;
-        }
-        
-        
         while(i<j){
-            if(list.get(i) + list.get(j) == x)
+            if(list.get(i)+list.get(j)==k)
                 return true;
             
-            else if(list.get(i) + list.get(j) < x)
+           else if(list.get(i)+list.get(j) < k)
                 i++;
             
             else
                 j--;
+            
         }
         
         return false;
