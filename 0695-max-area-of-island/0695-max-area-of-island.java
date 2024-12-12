a5133de0ff406e30f585ee766e8da6b9779552f5
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-12-12 11:38:00 +0530

    Time: 2 ms (69.69%), Space: 45.2 MB (11.72%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,15 +1,16 @@
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
         
-        int maxArea = 0;
         int row = grid.length;
         int col = grid[0].length;
+        
+        int maxArea = 0;
         
         for(int i=0; i<row; i++){
             for(int j=0; j<col; j++){
                 if(grid[i][j]==1){
-                    int getMax = dfs(grid, i, j);
-                    maxArea = Math.max(maxArea, getMax);
+                    int area = dfs(grid, i, j);
+                    maxArea = Math.max(maxArea, area);
                 }
             }
         }
@@ -20,7 +21,6 @@ class Solution {
     int dfs(int grid[][], int i, int j){
         if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]==2 || grid[i][j]==0)
             return 0;
-        
         
         grid[i][j] = 2;
         
