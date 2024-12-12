8500ba85bc979978805d5993bd29a8c303996737
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-12-12 11:21:25 +0530

    Time: 3 ms (86.92%), Space: 49.2 MB (75.21%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -9,8 +9,8 @@ class Solution {
         for(int i=0; i<row; i++){
             for(int j=0; j<col; j++){
                 if(grid[i][j]=='1'){
-                    countIsland++;
                     dfs(grid, i, j);
+                    countIsland++;
                 }
             }
         }
