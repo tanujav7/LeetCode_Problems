f45ca8f7017f382098ed18629291614f5717932f
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-12-12 13:50:48 +0530

    Time: 5 ms (98.55%), Space: 43.1 MB (58.69%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -11,14 +11,14 @@ class Solution {
         
         Arrays.sort(arr, (s1, s2)->(s2+s1).compareTo(s1+s2));
         
+        if(arr[0].equals("0"))
+            return "0";
+        
         StringBuilder sb = new StringBuilder();
         
         for(int i=0; i<n; i++){
             sb.append(arr[i]);
         }
-        
-        if(arr[0].equals("0"))
-            return "0";
         
         return sb.toString();
     }
