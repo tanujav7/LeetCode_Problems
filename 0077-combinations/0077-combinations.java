671339d9aab0d4c3ec0333085496e77df4eadb6a
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-12-12 11:04:45 +0530

    Time: 18 ms (81.09%), Space: 94.4 MB (66.62%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,11 +1,11 @@
 class Solution {
     List<List<Integer>> resList = new ArrayList<>();
     public List<List<Integer>> combine(int n, int k) {
-        backTrack(n, 1, k, new ArrayList<>());
+        backTrack(n, k, 1, new ArrayList<>());
         return resList;
     }
     
-    void backTrack(int n, int num, int k, List<Integer> list){
+    void backTrack(int n, int k, int num, List<Integer> list){
         if(k==list.size()){
             resList.add(new ArrayList<>(list));
             return;
@@ -13,7 +13,7 @@ class Solution {
         
         for(int i=num; i<=n; i++){
             list.add(i);
-            backTrack(n, i+1, k, list);
+            backTrack(n, k, i+1, list);
             list.remove(list.size()-1);
         }
     }
