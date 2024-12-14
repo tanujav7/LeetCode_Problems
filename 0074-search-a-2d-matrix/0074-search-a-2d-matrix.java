348717efd2d240fd2635c341473202015f560bac
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-12-14 21:34:28 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (92.82%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -2,7 +2,6 @@ class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         
         int row = matrix.length;
-        
         int col = matrix[0].length;
         
         int low = 0, high = row-1;
@@ -13,38 +12,35 @@ class Solution {
             int mid = low + (high-low)/2;
             
             if(matrix[mid][0]<=target && matrix[mid][col-1]>=target){
-                 targetRow = mid;
-                 break;
+                targetRow = mid;
+                break;
             }
-               
             
             else if(matrix[mid][0]<target)
                 low = mid+1;
             
             else
                 high = mid-1;
-                
         }
         
         
         low = 0;
         high = col-1;
         
-         while(low<=high){
+        while(low<=high){
             int mid = low + (high-low)/2;
             
-            if(matrix[targetRow][mid]==target)
+            if(matrix[targetRow][mid]==target){
                 return true;
+            }
             
             else if(matrix[targetRow][mid]<target)
                 low = mid+1;
             
             else
                 high = mid-1;
-                
         }
         
         return false;
-        
     }
 }
