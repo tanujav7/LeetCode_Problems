a97f49bb5283e9a4a3940ad1e1296d672a42cf4c
Tanuja V <73555975+tanujav7@users.noreply.github.com>
2024-12-15 19:08:44 +0530

    Time: 0 ms (100.00%), Space: 42 MB (61.41%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -9,36 +9,38 @@ class Solution {
         int targetRow = 0;
         
         while(low<=high){
+            
             int mid = low + (high-low)/2;
             
             if(matrix[mid][0]<=target && matrix[mid][col-1]>=target){
-                targetRow = mid;
+               targetRow = mid;
                 break;
             }
             
+            
             else if(matrix[mid][0]<target)
-                low = mid+1;
+                low = mid + 1;
             
             else
-                high = mid-1;
+                high = mid-1; 
         }
         
         
         low = 0;
         high = col-1;
-        
+
         while(low<=high){
+            
             int mid = low + (high-low)/2;
             
-            if(matrix[targetRow][mid]==target){
+            if(matrix[targetRow][mid]==target)
                 return true;
-            }
             
             else if(matrix[targetRow][mid]<target)
-                low = mid+1;
+                low = mid + 1;
             
             else
-                high = mid-1;
+                high = mid-1; 
         }
         
         return false;
